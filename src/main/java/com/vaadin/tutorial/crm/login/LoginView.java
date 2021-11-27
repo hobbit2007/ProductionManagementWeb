1e857210776084e4aa54802abd27336c1c21641d
r.grytsay <roman-grytsay@yandex.ru>
2021-11-27 19:27:49 +0300

    Форма авторизации

--- src/main/java/com/vaadin/tutorial/crm/login/LoginView.java
+++ src/main/java/com/vaadin/tutorial/crm/login/LoginView.java
@@ -2,43 +2,29 @@ package com.vaadin.tutorial.crm.login;
 
 import com.vaadin.flow.component.UI;
 import com.vaadin.flow.component.applayout.AppLayout;
-import com.vaadin.flow.component.button.Button;
 import com.vaadin.flow.component.html.Label;
 import com.vaadin.flow.component.login.LoginForm;
+import com.vaadin.flow.component.login.LoginI18n;
 import com.vaadin.flow.component.orderedlayout.FlexComponent;
-import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
-import com.vaadin.flow.component.textfield.PasswordField;
-import com.vaadin.flow.component.textfield.TextField;
 import com.vaadin.flow.router.PageTitle;
 import com.vaadin.flow.router.Route;
-import com.vaadin.tutorial.crm.entity.User;
 import com.vaadin.tutorial.crm.security.AuthManager;
 import com.vaadin.tutorial.crm.ui.MainView;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.ldap.NamingException;
-
-import javax.sql.DataSource;
 import java.io.Serializable;
-import java.util.List;
 
+/**
+ * Класс реализующий авторизацию в системе
+ */
 @Route("login")
-@PageTitle("Войти | Система управления несоответствиями")
+@PageTitle("Войти | Управление производством")
 public class LoginView extends AppLayout implements Serializable {
     VerticalLayout verticalLayout;
-    HorizontalLayout horizontalLayout;
-    TextField textLogin;
-    PasswordField textPasswd;
-    Button buttonLogin;
     FlexComponent.Alignment alignment;
-    Label label, label1;
+    Label label1;
 
-    JdbcTemplate jdbcTemplate = new JdbcTemplate();
-    List<User> userList;
-    private static DataSource dataSource;
-
-    // @Autowired
     private AuthManager authManager;
 
     @Autowired
@@ -46,27 +32,26 @@ public class LoginView extends AppLayout implements Serializable {
 
         this.authManager = authManager;
         verticalLayout = new VerticalLayout();
-        horizontalLayout = new HorizontalLayout();
         verticalLayout.setSizeFull();
 
-        label = new Label("Вход в систему");
-        label.getStyle().set("color", "black");
-        label.getStyle().set("font-weight", "bold");
-        label.getStyle().set("font-size", "25pt");
-
-        label1 = new Label("Система управления несоответствиями");
+        label1 = new Label("ИстАгроДон - Система управление производством");
         label1.getStyle().set("color", "blue");
         label1.getStyle().set("font-weight", "bold");
         label1.getStyle().set("font-size", "15pt");
         label1.getStyle().set("margin-left", "30px");
 
         LoginForm component = new LoginForm();
+        component.setForgotPasswordButtonVisible(false);
+        LoginI18n loginI18n = new LoginI18n();
+        LoginI18n.Form form = new LoginI18n.Form();
+        form.setUsername("Логин");
+        form.setPassword("Пароль");
+        form.setTitle("Вход в систему");
+        form.setSubmit("Войти");
+        loginI18n.setForm(form);
+        component.setI18n(loginI18n);
 
-        verticalLayout.add(label);
         verticalLayout.add(component);
-
-        horizontalLayout.setAlignItems(alignment.CENTER);
-        verticalLayout.add(horizontalLayout);
 
         addToNavbar(label1);
         verticalLayout.setAlignItems(alignment.CENTER);
@@ -74,11 +59,13 @@ public class LoginView extends AppLayout implements Serializable {
 
         component.addLoginListener(e -> {
             authenticateUser(e.getUsername(), e.getPassword());
+            component.setEnabled(true);
         });
     }
 
     public void authenticateUser(String login, String passwd) throws NamingException {
         authManager.login(login, passwd);
-        UI.getCurrent().navigate(MainView.class);
+        if (authManager.authFlag)
+            UI.getCurrent().navigate(MainView.class);
     }
 }

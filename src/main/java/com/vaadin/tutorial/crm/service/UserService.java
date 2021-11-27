1b74fde4173edc055218c953a10f5f823715320b
r.grytsay <roman-grytsay@yandex.ru>
2021-11-27 18:36:26 +0300

    Добавил класс интерфейс содержащий методы для работы с таблицей userslist

--- src/main/java/com/vaadin/tutorial/crm/service/UserService.java
+++ src/main/java/com/vaadin/tutorial/crm/service/UserService.java
@@ -1,7 +1,12 @@
 package com.vaadin.tutorial.crm.service;
 
+import com.vaadin.tutorial.crm.entity.User;
+
+import java.util.Optional;
+
 /**
  * Класс интерфейс содержащий методы для работы с таблицей userslist
  */
 public interface UserService {
+    Optional<User> getAll(String username);
 }

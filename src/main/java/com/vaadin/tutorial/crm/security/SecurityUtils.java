8d7bd963e11dec5b4c00096a4f74e6cf278c327f
r.grytsay <roman-grytsay@yandex.ru>
2021-11-27 15:32:05 +0300

    Изменил класс

--- src/main/java/com/vaadin/tutorial/crm/security/SecurityUtils.java
+++ src/main/java/com/vaadin/tutorial/crm/security/SecurityUtils.java
@@ -1,6 +1,6 @@
 package com.vaadin.tutorial.crm.security;
 
-import com.vaadin.flow.server.ServletHelper;
+import com.vaadin.flow.server.HandlerHelper;
 import com.vaadin.flow.shared.ApplicationConstants;
 import lombok.experimental.UtilityClass;
 import org.springframework.security.core.context.SecurityContextHolder;
@@ -17,7 +17,7 @@ public class SecurityUtils {
         final String parameterValue = request.getParameter
                 (ApplicationConstants.REQUEST_TYPE_PARAMETER);
         return parameterValue != null
-                && Stream.of(ServletHelper.RequestType.values())
+                && Stream.of(HandlerHelper.RequestType.values())
                 .anyMatch(r -> r.getIdentifier().equals(parameterValue));
     }
 

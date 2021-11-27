948b72758d9d22934e8fc002739116fbc4450c0f
r.grytsay <roman-grytsay@yandex.ru>
2021-11-27 15:13:28 +0300

    Добавил аннотацию

--- src/main/java/com/vaadin/tutorial/crm/repository/UserRepository.java
+++ src/main/java/com/vaadin/tutorial/crm/repository/UserRepository.java
@@ -2,10 +2,15 @@ package com.vaadin.tutorial.crm.repository;
 
 import com.vaadin.tutorial.crm.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 
 import java.util.Optional;
 
+/**
+ * Класс интерфейс содержащий sql запросы к таблице userslist
+ */
+@Repository
 public interface UserRepository extends JpaRepository<User, Long> {
     Optional<User> findByLogin(String userName);
 }

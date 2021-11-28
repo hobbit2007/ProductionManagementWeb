e3922d9137f27194910e76fedfa27f1b468890f3
r.grytsay <roman-grytsay@yandex.ru>
2021-11-28 14:19:02 +0300

    Класс модель описывающий таблицу shop

---
+++ src/main/java/com/vaadin/tutorial/crm/entity/Shop.java
@@ -0,0 +1,25 @@
+package com.vaadin.tutorial.crm.entity;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+/**
+ * Класс модель описывающий таблицу shop
+ */
+@Entity(name = "shop")
+@Getter
+@Setter
+public class Shop {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    private String shop_name;
+
+    private Long delete;
+}

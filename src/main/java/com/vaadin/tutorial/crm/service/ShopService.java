712ec4e1a2548d31385ed9339d1da4bb1170cd29
r.grytsay <roman-grytsay@yandex.ru>
2021-11-28 14:40:58 +0300

    Добавил аннотацию

--- src/main/java/com/vaadin/tutorial/crm/service/ShopService.java
+++ src/main/java/com/vaadin/tutorial/crm/service/ShopService.java
@@ -1,12 +1,14 @@
 package com.vaadin.tutorial.crm.service;
 
 import com.vaadin.tutorial.crm.entity.Shop;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 
 /**
  * Класс интерфейс содержащий методы для работы с таблицей shop
  */
+@Service
 public interface ShopService {
     List<Shop> getAll();
 }

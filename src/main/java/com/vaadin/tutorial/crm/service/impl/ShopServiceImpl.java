3f2827b732ae9c240aa36d54baf5163455095d39
r.grytsay <roman-grytsay@yandex.ru>
2021-11-28 14:38:51 +0300

    Класс реализующий методы интерфейса ShopService

---
+++ src/main/java/com/vaadin/tutorial/crm/service/impl/ShopServiceImpl.java
@@ -0,0 +1,25 @@
+package com.vaadin.tutorial.crm.service.impl;
+
+import com.vaadin.tutorial.crm.entity.Shop;
+import com.vaadin.tutorial.crm.repository.ShopRepository;
+import com.vaadin.tutorial.crm.service.ShopService;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.List;
+
+/**
+ * Класс реализующий методы интерфейса ShopService
+ */
+public class ShopServiceImpl implements ShopService {
+    private ShopRepository shopRepository;
+
+    @Autowired
+    public ShopServiceImpl(ShopRepository shopRepository) {
+        this.shopRepository = shopRepository;
+    }
+
+    @Override
+    public List<Shop> getAll() {
+        return shopRepository.getAll();
+    }
+}

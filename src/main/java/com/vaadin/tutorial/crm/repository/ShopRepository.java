88f58297b1e9716ee12c94e56de20206a8694577
r.grytsay <roman-grytsay@yandex.ru>
2021-11-28 14:31:50 +0300

    Класс репозиторий содержащий sql запросы к таблице shop

---
+++ src/main/java/com/vaadin/tutorial/crm/repository/ShopRepository.java
@@ -0,0 +1,17 @@
+package com.vaadin.tutorial.crm.repository;
+
+import com.vaadin.tutorial.crm.entity.Shop;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+/**
+ * Класс репозиторий содержащий sql запросы к таблице shop
+ */
+@Repository
+public interface ShopRepository extends JpaRepository<Shop, Long> {
+    @Query("select s from shop s where s.delete = 0")
+    List<Shop> getAll();
+}

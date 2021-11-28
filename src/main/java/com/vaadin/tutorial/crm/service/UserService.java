14cb84180d5d271739c02b89b91f5a2c3733ac20
r.grytsay <roman-grytsay@yandex.ru>
2021-11-28 14:40:29 +0300

    Добавил аннотацию

--- src/main/java/com/vaadin/tutorial/crm/service/UserService.java
+++ src/main/java/com/vaadin/tutorial/crm/service/UserService.java
@@ -1,12 +1,14 @@
 package com.vaadin.tutorial.crm.service;
 
 import com.vaadin.tutorial.crm.entity.User;
+import org.springframework.stereotype.Service;
 
 import java.util.Optional;
 
 /**
  * Класс интерфейс содержащий методы для работы с таблицей userslist
  */
+@Service
 public interface UserService {
     Optional<User> getAll(String username);
 

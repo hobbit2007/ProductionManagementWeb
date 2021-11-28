5640eaa50a05d3d65017f2f5e5ae10dbfada933a
r.grytsay <roman-grytsay@yandex.ru>
2021-11-28 14:21:43 +0300

    Класс модель описывающий таблицу department

---
+++ src/main/java/com/vaadin/tutorial/crm/entity/Department.java
@@ -0,0 +1,29 @@
+package com.vaadin.tutorial.crm.entity;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+/**
+ * Класс модель описывающий таблицу department
+ */
+@Entity(name = "department")
+@Getter
+@Setter
+public class Department {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    private String departmentName;
+
+    private Long shopID;
+
+    private Long delete;
+
+
+}

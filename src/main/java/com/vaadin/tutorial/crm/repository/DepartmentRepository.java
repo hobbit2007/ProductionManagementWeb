1ec3659866c9d5db87755b4d40dd529ae1b58260
r.grytsay <roman-grytsay@yandex.ru>
2021-11-28 14:43:52 +0300

    Класс репозиторий содержащий sql запросы для работы с таблицей department

---
+++ src/main/java/com/vaadin/tutorial/crm/repository/DepartmentRepository.java
@@ -0,0 +1,17 @@
+package com.vaadin.tutorial.crm.repository;
+
+import com.vaadin.tutorial.crm.entity.Department;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+/**
+ * Класс репозиторий содержащий sql запросы для работы с таблицей department
+ */
+@Repository
+public interface DepartmentRepository extends JpaRepository<Department, Long> {
+    @Query("select d from department d where d.delete = 0")
+    List<Department> getAll();
+}

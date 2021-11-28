a3a980eb6224a858a84490339540429149d37bbe
r.grytsay <roman-grytsay@yandex.ru>
2021-11-28 14:40:03 +0300

    Добавил аннотацию

--- src/main/java/com/vaadin/tutorial/crm/service/impl/ShopServiceImpl.java
+++ src/main/java/com/vaadin/tutorial/crm/service/impl/ShopServiceImpl.java
@@ -4,12 +4,14 @@ import com.vaadin.tutorial.crm.entity.Shop;
 import com.vaadin.tutorial.crm.repository.ShopRepository;
 import com.vaadin.tutorial.crm.service.ShopService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 
 /**
  * Класс реализующий методы интерфейса ShopService
  */
+@Service
 public class ShopServiceImpl implements ShopService {
     private ShopRepository shopRepository;
 

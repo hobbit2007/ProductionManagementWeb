abf645adff8b56be1b7f136c7802af19db665e0d
Roman Grytsay <r.grytsay@platinka.ru>
2021-11-29 16:45:02 +0300

    Изменил имя класса

--- src/main/java/com/vaadin/tutorial/crm/ui/layout/PlcLayout.java
+++ src/main/java/com/vaadin/tutorial/crm/ui/layout/PlcLayout.java
@@ -6,6 +6,7 @@ import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.router.HighlightConditions;
 import com.vaadin.flow.router.RouterLink;
 import com.vaadin.tutorial.crm.security.SecurityConfiguration;
+import com.vaadin.tutorial.crm.ui.MainView;
 import com.vaadin.tutorial.crm.ui.component.LabelComponent;
 import com.vaadin.tutorial.crm.ui.plccontrollersui.PlcWashingController;
 
@@ -29,32 +30,32 @@ public class PlcLayout extends AppLayout {
     private void createDrawer() {
         RouterLink plcWashingRT = new RouterLink("PLC контроллер Мойка(реальное время)", PlcWashingController.class);
         plcWashingRT.setHighlightCondition(HighlightConditions.sameLocation());
-        RouterLink plcWashingStorage = new RouterLink("PLC контроллер Мойка(хранилище)", PlcWashingController.class);
+        RouterLink plcWashingStorage = new RouterLink("PLC контроллер Мойка(хранилище)", MainView.class);
 
         plcWashingStorage.setHighlightCondition(HighlightConditions.sameLocation());
-        RouterLink plcDiffusionRT = new RouterLink("PLC контроллер Диффузия(реальное время)", PlcWashingController.class);
+        RouterLink plcDiffusionRT = new RouterLink("PLC контроллер Диффузия(реальное время)", MainView.class);
         plcDiffusionRT.setHighlightCondition(HighlightConditions.sameLocation());
-        RouterLink plcDiffusionStorage = new RouterLink("PLC контроллер Диффузия(хранилище)", PlcWashingController.class);
+        RouterLink plcDiffusionStorage = new RouterLink("PLC контроллер Диффузия(хранилище)", MainView.class);
         plcDiffusionStorage.setHighlightCondition(HighlightConditions.sameLocation());
 
-        RouterLink plcFermentationRT = new RouterLink("PLC контроллер Ферментация(реальное время)", PlcWashingController.class);
+        RouterLink plcFermentationRT = new RouterLink("PLC контроллер Ферментация(реальное время)", MainView.class);
         plcFermentationRT.setHighlightCondition(HighlightConditions.sameLocation());
-        RouterLink plcFermentationStorage = new RouterLink("PLC контроллер Ферментация(хранилище)", PlcWashingController.class);
+        RouterLink plcFermentationStorage = new RouterLink("PLC контроллер Ферментация(хранилище)", MainView.class);
         plcFermentationStorage.setHighlightCondition(HighlightConditions.sameLocation());
 
-        RouterLink plcResidueRT = new RouterLink("PLC контроллер Выпарка(реальное время)", PlcWashingController.class);
+        RouterLink plcResidueRT = new RouterLink("PLC контроллер Выпарка(реальное время)", MainView.class);
         plcResidueRT.setHighlightCondition(HighlightConditions.sameLocation());
-        RouterLink plcResidueStorage = new RouterLink("PLC контроллер Выпарка(хранилище)", PlcWashingController.class);
+        RouterLink plcResidueStorage = new RouterLink("PLC контроллер Выпарка(хранилище)", MainView.class);
         plcResidueStorage.setHighlightCondition(HighlightConditions.sameLocation());
 
-        RouterLink plcRollingRT = new RouterLink("PLC контроллер Розлив сиропа(реальное время)", PlcWashingController.class);
+        RouterLink plcRollingRT = new RouterLink("PLC контроллер Розлив сиропа(реальное время)", MainView.class);
         plcRollingRT.setHighlightCondition(HighlightConditions.sameLocation());
-        RouterLink plcRollingStorage = new RouterLink("PLC контроллер Розлив сиропа(хранилище)", PlcWashingController.class);
+        RouterLink plcRollingStorage = new RouterLink("PLC контроллер Розлив сиропа(хранилище)", MainView.class);
         plcRollingStorage.setHighlightCondition(HighlightConditions.sameLocation());
 
-        RouterLink plcGranulatorRT = new RouterLink("PLC контроллер Гранулятор(реальное время)", PlcWashingController.class);
+        RouterLink plcGranulatorRT = new RouterLink("PLC контроллер Гранулятор(реальное время)", MainView.class);
         plcGranulatorRT.setHighlightCondition(HighlightConditions.sameLocation());
-        RouterLink plcGranulatorStorage = new RouterLink("PLC контроллер Гранулятор(хранилище)", PlcWashingController.class);
+        RouterLink plcGranulatorStorage = new RouterLink("PLC контроллер Гранулятор(хранилище)", MainView.class);
         plcGranulatorStorage.setHighlightCondition(HighlightConditions.sameLocation());
 
         addToDrawer(new VerticalLayout(plcWashingRT, plcWashingStorage, plcDiffusionRT, plcDiffusionStorage, plcFermentationRT, plcFermentationStorage,

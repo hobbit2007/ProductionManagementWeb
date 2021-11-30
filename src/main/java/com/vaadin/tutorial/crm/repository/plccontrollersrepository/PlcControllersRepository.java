328cc17149ee23a94bf3a07c5b96f888d719f22f
Roman Grytsay <r.grytsay@platinka.ru>
2021-11-30 09:38:19 +0300

    Класс репозиторий содержащий sql запросы к таблице plccontrollers

---
+++ src/main/java/com/vaadin/tutorial/crm/repository/plccontrollersrepository/PlcControllersRepository.java
@@ -0,0 +1,17 @@
+package com.vaadin.tutorial.crm.repository.plccontrollersrepository;
+
+import com.vaadin.tutorial.crm.entity.plccontrollersentity.PlcControllers;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+/**
+ * Класс репозиторий содержащий sql запросы к таблице plccontrollers
+ */
+@Repository
+public interface PlcControllersRepository extends JpaRepository<PlcControllers, Long> {
+    @Query("select pc from plccontrollers pc where pc.delete = 0")
+    List<PlcControllers> getAll();
+}

37a26431af25d4fc4827ce16812b44b4eed825c5
Roman Grytsay <r.grytsay@platinka.ru>
2021-11-30 11:14:02 +0300

    Класс репозиторий содержащий sql запросы к таблице signallist

---
+++ src/main/java/com/vaadin/tutorial/crm/repository/plccontrollersrepository/SignalListRepository.java
@@ -0,0 +1,18 @@
+package com.vaadin.tutorial.crm.repository.plccontrollersrepository;
+
+import com.vaadin.tutorial.crm.entity.plccontrollersentity.SignalList;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+/**
+ * Класс репозиторий содержащий sql запросы к таблице signallist
+ */
+@Repository
+public interface SignalListRepository extends JpaRepository<SignalList, Long> {
+    @Query("select sl from signallist sl where sl.idController = :controllerId and sl.delete = 0")
+    List<SignalList> findSignalList(@Param("controllerId") Long controllerId);
+}

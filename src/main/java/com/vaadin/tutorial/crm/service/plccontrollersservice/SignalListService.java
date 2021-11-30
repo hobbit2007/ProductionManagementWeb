7903ae20909331e221b2aa75d93c366e76fd60cb
Roman Grytsay <r.grytsay@platinka.ru>
2021-11-30 11:18:15 +0300

    Исправил пакет

--- src/main/java/com/vaadin/tutorial/crm/service/plccontrollersservice/SignalListService.java
+++ src/main/java/com/vaadin/tutorial/crm/service/plccontrollersservice/SignalListService.java
@@ -1,4 +1,4 @@
-package com.vaadin.tutorial.crm.service;
+package com.vaadin.tutorial.crm.service.plccontrollersservice;
 
 import com.vaadin.tutorial.crm.entity.plccontrollersentity.SignalList;
 import org.springframework.stereotype.Service;

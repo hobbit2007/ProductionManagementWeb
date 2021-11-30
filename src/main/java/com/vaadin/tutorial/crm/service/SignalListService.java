dd92a9ed1234cd74f9f355428dde848920e7991e
Roman Grytsay <r.grytsay@platinka.ru>
2021-11-30 11:17:07 +0300

    Класс модель содержащий методы для работы с таблицей signallist

---
+++ src/main/java/com/vaadin/tutorial/crm/service/SignalListService.java
@@ -0,0 +1,14 @@
+package com.vaadin.tutorial.crm.service;
+
+import com.vaadin.tutorial.crm.entity.plccontrollersentity.SignalList;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+/**
+ * Класс модель содержащий методы для работы с таблицей signallist
+ */
+@Service
+public interface SignalListService {
+    List<SignalList> findSignalList(Long controllerId);
+}

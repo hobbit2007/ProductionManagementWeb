3e079ca85b49b401bbc923cda7b97c1a13c157f5
Roman Grytsay <r.grytsay@platinka.ru>
2021-11-30 09:35:01 +0300

    Класс модель описывающая таблицу plccontrollers

---
+++ src/main/java/com/vaadin/tutorial/crm/entity/plccontrollersentity/PlcController.java
@@ -0,0 +1,43 @@
+package com.vaadin.tutorial.crm.entity.plccontrollersentity;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import java.io.Externalizable;
+import java.io.IOException;
+import java.io.ObjectInput;
+import java.io.ObjectOutput;
+import java.util.Date;
+
+/**
+ * Класс модель описывающая таблицу plccontrollers
+ */
+@Entity(name = "plccontrollers")
+@Getter
+@Setter
+public class PlcController implements Externalizable {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+    private String controllerName;
+    private String controllerDescription;
+    private String ip;
+    private String mask;
+    private Long idUserCreate;
+    private Date dateCreate;
+    private Long delete;
+
+    @Override
+    public void writeExternal(ObjectOutput objectOutput) throws IOException {
+
+    }
+
+    @Override
+    public void readExternal(ObjectInput objectInput) throws IOException, ClassNotFoundException {
+
+    }
+}

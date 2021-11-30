1ae7f3cf17a2de23a8ce44226a921957bb8811fe
Roman Grytsay <r.grytsay@platinka.ru>
2021-11-30 11:09:18 +0300

    Класс модель описывающий таблицу signallist

---
+++ src/main/java/com/vaadin/tutorial/crm/entity/plccontrollersentity/SignalList.java
@@ -0,0 +1,45 @@
+package com.vaadin.tutorial.crm.entity.plccontrollersentity;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import java.io.Externalizable;
+import java.io.IOException;
+import java.io.ObjectInput;
+import java.io.ObjectOutput;
+import java.util.Date;
+
+/**
+ * Класс модель описывающий таблицу signallist
+ */
+@Entity(name = "signallist")
+@Getter
+@Setter
+public class SignalList implements Externalizable {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String signalName;
+    private String signalDescription;
+    private Long delete;
+    private Long dbValue;
+    private Long position;
+    private int offset;
+    private Long idUserCreate;
+    private Date dateCreate;
+    private Long idController;
+
+    @Override
+    public void writeExternal(ObjectOutput objectOutput) throws IOException {
+
+    }
+
+    @Override
+    public void readExternal(ObjectInput objectInput) throws IOException, ClassNotFoundException {
+
+    }
+}

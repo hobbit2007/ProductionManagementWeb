c2e027a5266df38f256e26ca1dda97f01e68f030
Roman Grytsay <r.grytsay@platinka.ru>
2021-12-01 15:48:14 +0300

    Обновил имя

--- src/main/java/com/vaadin/tutorial/crm/ui/layout/UserLayout.java
+++ src/main/java/com/vaadin/tutorial/crm/ui/layout/UserLayout.java
@@ -7,14 +7,16 @@ import com.vaadin.flow.router.HighlightConditions;
 import com.vaadin.flow.router.RouterLink;
 import com.vaadin.tutorial.crm.security.SecurityConfiguration;
 import com.vaadin.tutorial.crm.ui.MainView;
+import com.vaadin.tutorial.crm.ui.admin.AdminPage;
 import com.vaadin.tutorial.crm.ui.component.LabelComponent;
-import com.vaadin.tutorial.crm.ui.users.UserCreate;
+import com.vaadin.tutorial.crm.ui.users.UsersCreate;
 
 /**
  * Класс реализующий боковое меню управления пользователями
  */
 @CssImport("./styles/shared-styles.css")
 public class UserLayout extends AppLayout {
+
     private final SecurityConfiguration securityConfiguration;
 
     LabelComponent labelComponent;
@@ -30,16 +32,16 @@ public class UserLayout extends AppLayout {
     }
 
     private void createDrawer() {
-        RouterLink back = new RouterLink("Назад", AdminLayout.class);
+        RouterLink back = new RouterLink("Назад", AdminPage.class);
         back.setHighlightCondition(HighlightConditions.sameLocation());
 
-        RouterLink userCreate = new RouterLink("Создать пользователя", UserCreate.class);
-        userCreate.setHighlightCondition(HighlightConditions.sameLocation());
+        RouterLink usersCreate = new RouterLink("Создать пользователя", UsersCreate.class);
+        usersCreate.setHighlightCondition(HighlightConditions.sameLocation());
 
         RouterLink userEdit = new RouterLink("Редактировать пользователя", MainView.class);
         userEdit.setHighlightCondition(HighlightConditions.sameLocation());
 
-        addToDrawer(new VerticalLayout(back, userCreate, userEdit));
+        addToDrawer(new VerticalLayout(back, usersCreate, userEdit));
 
         //Закрываем меню на стороне клиента
         //т.к. при первом запуске меню показывается автоматически

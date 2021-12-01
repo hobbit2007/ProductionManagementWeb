3b6e0a582ed4983f3d5bd00cc80d7fc1ad555f5f
Roman Grytsay <r.grytsay@platinka.ru>
2021-12-01 16:24:16 +0300

    Добавил метод вызова потока

--- src/main/java/com/vaadin/tutorial/crm/ui/MainView.java
+++ src/main/java/com/vaadin/tutorial/crm/ui/MainView.java
@@ -1,14 +1,22 @@
 package com.vaadin.tutorial.crm.ui;
 
+import com.vaadin.flow.component.AttachEvent;
+import com.vaadin.flow.component.AttachNotifier;
 import com.vaadin.flow.component.applayout.AppLayout;
 import com.vaadin.flow.component.dependency.CssImport;
+import com.vaadin.flow.component.html.H1;
 import com.vaadin.flow.component.html.Label;
 import com.vaadin.flow.component.orderedlayout.FlexComponent;
 import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+
+import com.vaadin.flow.component.page.Push;
 import com.vaadin.flow.router.PageTitle;
 import com.vaadin.flow.router.Route;
+import com.vaadin.tutorial.crm.service.plccontrollersservice.SchedulerService;
+import com.vaadin.tutorial.crm.threads.FeederThread;
 import com.vaadin.tutorial.crm.ui.layout.MainLayout;
+import org.springframework.scheduling.annotation.Scheduled;
 
 /**
  * Главный класс приложения
@@ -17,14 +25,13 @@ import com.vaadin.tutorial.crm.ui.layout.MainLayout;
 @PageTitle("Dashboard | Система управления производством")
 @CssImport("./styles/shared-styles.css")
 @CssImport(value = "./styles/vaadin-text-field-styles.css", themeFor = "vaadin-text-field")
-public class MainView extends AppLayout {
+public class MainView extends VerticalLayout {
 
-    Label labelUser;
+    private Label labelUser  = new Label();
     VerticalLayout vContent = new VerticalLayout();
     HorizontalLayout hContent = new HorizontalLayout();
 
     public MainView() {
-        labelUser = new Label("ЗДЕСЬ БУДУТ ДАШБОАРДЫ");
         labelUser.getStyle().set("color", "red");
         labelUser.getStyle().set("font-weight", "bold");
         labelUser.getStyle().set("font-size", "11pt");
@@ -38,8 +45,14 @@ public class MainView extends AppLayout {
         vContent.setSizeFull();
 
 
-        setContent(vContent);
+        add(vContent);
 
     }
 
+    @Override
+    protected void onAttach(AttachEvent attachEvent) {
+        // Start the data feed thread
+        FeederThread thread = new FeederThread(attachEvent.getUI(), labelUser);
+        thread.start();
+    }
 }

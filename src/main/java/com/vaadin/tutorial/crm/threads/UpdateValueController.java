6cd2fe5d80cfdadfd72185141fd1e5c2e109afe0
Roman Grytsay <r.grytsay@platinka.ru>
2021-12-02 18:46:26 +0300

    Далаю работу с потоками

--- src/main/java/com/vaadin/tutorial/crm/threads/UpdateValueController.java
+++ src/main/java/com/vaadin/tutorial/crm/threads/UpdateValueController.java
@@ -1,43 +1,96 @@
 package com.vaadin.tutorial.crm.threads;
 
+import com.sourceforge.snap7.moka7.S7;
+import com.sourceforge.snap7.moka7.S7Client;
 import com.vaadin.flow.component.UI;
+import com.vaadin.flow.component.html.Label;
 import com.vaadin.flow.component.textfield.TextField;
+import com.vaadin.tutorial.crm.entity.plccontrollersentity.SignalList;
+import com.vaadin.tutorial.crm.model.DataFromPlc;
 import com.vaadin.tutorial.crm.service.plccontrollersservice.SchedulerService;
+
+import java.util.ArrayList;
+import java.util.List;
+
 /**
 * Класс поток - проверяет подключение к контроллерам каждые 10 сек.
 */
 public class UpdateValueController extends Thread {
     private final UI ui;
-    private  final TextField element;
-    private final int size;
-
-    private int count = 30;
+    private  List<TextField> element;
 
 
-    public UpdateValueController(UI ui, TextField element, int size) {
+    public static List<DataFromPlc> dataFromPlcList = new ArrayList<>(); //Массив в котором сохраняем данные после чтения из контроллера
+    private boolean controllerConnect = true;
+    private int db;
+    private int pos;
+    private int offset;
+    private String controllerIpAddress;
+    private float readData;
+    private List<DataFromPlc> array;
+    int count = 10;
+
+
+    public UpdateValueController(UI ui, List<TextField> element, List<DataFromPlc> array) {//int db, int pos, int offset, String controllerIpAddress
         this.ui = ui;
         this.element = element;
-        this.size = size;
+        this.array = array;
+        //this.db = db;
+        //this.pos = pos;
+        //this.offset = offset;
+        //this.controllerIpAddress = controllerIpAddress;
+
+
+
     }
 
     @Override
     public void run() {
-        while (true){
+        //S7Client clientRT = new S7Client();
+        //clientRT.SetConnectionType(S7.OP);
+        //clientRT.ConnectTo(controllerIpAddress, 0, 1);
+        //byte[] buffer = new byte[65536];
+       // while (true){ //count > 0
             try {
-                Thread.sleep(1000);
-                ui.access(()-> {
-                    for (int i = 0; i < size; i++) {
-                        if (SchedulerService.dataFromPlcList.size() != 0) {
-                            element.setValue(String.valueOf(SchedulerService.dataFromPlcList.get(i).getValue()));
+
+
+
+                //sleep(1000);
+                //DataFromPlc dataFromPlc = new DataFromPlc();
+                //for (int i = 0; i < numDbPosOffset.size(); i++) {
+                //if (clientRT.Connected) {
+                //clientRT.ReadArea(S7.S7AreaDB, db, 0, pos + offset, buffer);
+                //    readData = S7.GetFloatAt(buffer, pos);
+                    //dataFromPlc.setSignalName("Signal");
+                    //dataFromPlc.setValue(readData);
+                    //dataFromPlcList.add(dataFromPlc);
+                    //}
+                    //Thread.sleep(3000);
+                    //System.out.println("VALUE"+getId()+" - " + getName() + " = " + readData);
+                    //sleep(3000);
+                int test = element.size();
+                int test1 = array.size();
+                    ui.access(() -> {
+                        //if (element.size() == SchedulerService.dataFromPlcList.size()) {
+                        for (int i = 0; i < element.size(); i++) {
+                            element.get(i).setValue(String.valueOf(array.get(i).getValue()));//dataFromPlcList.get(0).getValue())
                             ui.push();
-                            System.out.println(SchedulerService.dataFromPlcList.get(i).getSignalName() + "=" + SchedulerService.dataFromPlcList.get(i).getValue());
                         }
-                    }
-                });
+                        //System.out.println("VALUE"+getId()+" - " + getName() + " = " + readData);
+                        //}
+                    });
+
+                    //System.out.println("------------------------NEW COUNT----------------------------");
+                //}
+                //else {
+                //    return;
+                //}
+                //dataFromPlcList.removeAll(dataFromPlcList);
+           //     sleep(3000);
                 count--;
-            } catch (InterruptedException e) {
+            } catch (Exception e) { //Interrupted
                 e.printStackTrace();
-            }
+         //   }
         }
     }
 }

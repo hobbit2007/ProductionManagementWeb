ab8c862cc3a812277f0d4486b71c420005255440
r.grytsay <roman-grytsay@yandex.ru>
2021-12-04 13:37:38 +0300

    Добавил иконки и роли

--- src/main/java/com/vaadin/tutorial/crm/ui/layout/MainLayout.java
+++ src/main/java/com/vaadin/tutorial/crm/ui/layout/MainLayout.java
@@ -2,6 +2,9 @@ package com.vaadin.tutorial.crm.ui.layout;
 
 import com.vaadin.flow.component.applayout.AppLayout;
 import com.vaadin.flow.component.dependency.CssImport;
+import com.vaadin.flow.component.icon.Icon;
+import com.vaadin.flow.component.icon.VaadinIcon;
+import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.router.HighlightConditions;
 import com.vaadin.flow.router.RouterLink;
@@ -19,6 +22,9 @@ import com.vaadin.tutorial.crm.ui.plccontrollersui.PlcValueController;
 public class MainLayout extends AppLayout {
     private final SecurityConfiguration securityConfiguration;
     LabelComponent labelComponent;
+    HorizontalLayout hMenu1 = new HorizontalLayout();
+    HorizontalLayout hMenu2 = new HorizontalLayout();
+    HorizontalLayout hMenu3 = new HorizontalLayout();
 
     public MainLayout(SecurityConfiguration securityConfiguration) {
         this.securityConfiguration = securityConfiguration;
@@ -32,12 +38,20 @@ public class MainLayout extends AppLayout {
     private void createDrawer() {
         RouterLink dashboardLink = new RouterLink("Dashboard", MainView.class);
         dashboardLink.setHighlightCondition(HighlightConditions.sameLocation());
+        Icon icon = new Icon(VaadinIcon.DASHBOARD);
+        hMenu1.add(icon, dashboardLink);
+
         RouterLink plcLink = new RouterLink("PLC контроллеры", PlcValueController.class);
         plcLink.setHighlightCondition(HighlightConditions.sameLocation());
+        Icon icon1 = new Icon(VaadinIcon.CONTROLLER);
+        hMenu2.add(icon1, plcLink);
+
         RouterLink adminLink = new RouterLink("Администрирование", AdminPage.class);
         adminLink.setHighlightCondition(HighlightConditions.sameLocation());
+        Icon icon2 = new Icon(VaadinIcon.TOOLS);
+        hMenu3.add(icon2, adminLink);
 
-        addToDrawer(new VerticalLayout(dashboardLink, plcLink, adminLink));
+        addToDrawer(new VerticalLayout(hMenu1, hMenu2, hMenu3));
 
         //Закрываем меню на стороне клиента
         //т.к. при первом запуске меню показывается автоматически

5a1d025c87f4164fcbc7f4eaacb55f1c2331c534
r.grytsay <roman-grytsay@yandex.ru>
2021-12-05 16:08:23 +0300

    Добавил останов потока при покидании страницы

--- src/main/java/com/vaadin/tutorial/crm/ui/MainView.java
+++ src/main/java/com/vaadin/tutorial/crm/ui/MainView.java
@@ -2,8 +2,11 @@ package com.vaadin.tutorial.crm.ui;
 
 import com.vaadin.flow.component.AttachEvent;
 import com.vaadin.flow.component.AttachNotifier;
+import com.vaadin.flow.component.Component;
 import com.vaadin.flow.component.DetachEvent;
 import com.vaadin.flow.component.applayout.AppLayout;
+import com.vaadin.flow.component.charts.Chart;
+import com.vaadin.flow.component.charts.model.*;
 import com.vaadin.flow.component.dependency.CssImport;
 import com.vaadin.flow.component.html.H1;
 import com.vaadin.flow.component.html.Label;
@@ -14,10 +17,16 @@ import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.component.page.Push;
 import com.vaadin.flow.router.PageTitle;
 import com.vaadin.flow.router.Route;
+import com.vaadin.tutorial.crm.model.DataFromPlc;
 import com.vaadin.tutorial.crm.service.plccontrollersservice.SchedulerService;
 import com.vaadin.tutorial.crm.threads.FeederThread;
+import com.vaadin.tutorial.crm.ui.chart.SplineUpdatingEachSecond;
 import com.vaadin.tutorial.crm.ui.layout.MainLayout;
 import org.springframework.scheduling.annotation.Scheduled;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
 
 /**
  * Главный класс приложения
@@ -32,6 +41,9 @@ public class MainView extends VerticalLayout {
     VerticalLayout vContent = new VerticalLayout();
     HorizontalLayout hContent = new HorizontalLayout();
     FeederThread thread;
+    //SplineUpdatingEachSecond splineUpdatingEachSecond = new SplineUpdatingEachSecond();
+    Chart chart = new Chart();
+    public static List<DataFromPlc> arrayForChart = new ArrayList<>();
 
     public MainView() {
         SchedulerService.stopThread = true;
@@ -43,7 +55,7 @@ public class MainView extends VerticalLayout {
 
         hContent.add(labelUser);
 
-        vContent.add(hContent);
+        vContent.add(hContent, initDemo());
         vContent.setDefaultHorizontalComponentAlignment(FlexComponent.Alignment.CENTER);
 
         vContent.setSizeFull();
@@ -53,6 +65,49 @@ public class MainView extends VerticalLayout {
 
     }
 
+    public Component initDemo() {
+       // final Random random = new Random();
+
+
+
+        final Configuration configuration = chart.getConfiguration();
+        configuration.getChart().setType(ChartType.SPLINE);
+        configuration.getTitle().setText("Live data");
+
+        XAxis xAxis = configuration.getxAxis();
+        xAxis.setType(AxisType.DATETIME);
+        xAxis.setTickPixelInterval(150);
+
+        YAxis yAxis = configuration.getyAxis();
+        yAxis.setTitle(new AxisTitle("Value"));
+
+        configuration.getTooltip().setEnabled(false);
+        configuration.getLegend().setEnabled(false);
+
+        final DataSeries series = new DataSeries();
+        series.setPlotOptions(new PlotOptionsSpline());
+        series.setName("Random data");
+        if (arrayForChart.size() != 0) {
+            for (int i = 0; i <= arrayForChart.size(); i++) {
+                series.add(new DataSeriesItem(System.currentTimeMillis() + i * 1000, arrayForChart.get(i).getValue()));
+            }
+
+            configuration.setSeries(series);
+        }
+
+       /* runWhileAttached(chart, () -> {
+            final long x = System.currentTimeMillis();
+            final double y = random.nextDouble();
+            series.add(new DataSeriesItem(x, y), true, true);
+        }, 1000, 1000);*/
+
+        return chart;
+    }
+
+    public static void dataForControllerChart(List<DataFromPlc> array) {
+        arrayForChart = array;
+    }
+
     @Override
     protected void onAttach(AttachEvent attachEvent) {
         // Start the data feed thread
@@ -60,10 +115,10 @@ public class MainView extends VerticalLayout {
         thread.start();
     }
 
-    //@Override
-    //protected void onDetach(DetachEvent detachEvent) {
+    @Override
+    protected void onDetach(DetachEvent detachEvent) {
         // Cleanup
-    //    thread.interrupt();
-    //    thread = null;
-    //}
+        thread.interrupt();
+        thread = null;
+    }
 }

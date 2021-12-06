9be33e2afff17fd5e15bd29eae59f209282ca2c1
Roman Grytsay <r.grytsay@platinka.ru>
2021-12-06 14:28:09 +0300

    Рисую графики

--- src/main/java/com/vaadin/tutorial/crm/ui/chart/UpdateValueChart.java
+++ src/main/java/com/vaadin/tutorial/crm/ui/chart/UpdateValueChart.java
@@ -7,6 +7,8 @@ import com.vaadin.flow.component.charts.model.DataSeries;
 import com.vaadin.flow.component.charts.model.DataSeriesItem;
 import com.vaadin.tutorial.crm.model.DataFromPlc;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 /**
@@ -17,6 +19,8 @@ public class UpdateValueChart extends Thread{
     private Configuration configuration;
     private DataSeries series;
     private List<DataFromPlc> array;
+    private List<Float> dataPLC = new ArrayList<>(50);
+
 
     public UpdateValueChart(UI ui, Configuration configuration, DataSeries series, List<DataFromPlc> array) {
         this.ui = ui;
@@ -26,15 +30,19 @@ public class UpdateValueChart extends Thread{
     }
     @Override
     public void run() {
+
+
         try {
             ui.access(() -> {
-                for (int i = 0; i < array.size(); i++) {
-                    series.add(new DataSeriesItem(System.currentTimeMillis() + i * 1000, array.get(i).getValue()));
-                    //ui.push();
+                if (array.size() != 0) {
+                    for (int i = 0; i < array.size(); i++) {
+                        series.add(new DataSeriesItem(System.currentTimeMillis() + i * 1000, array.get(i).getValue()), true, true);
+                        //ui.push();
+                    }
+                    System.out.println("ARRAYSIZE = " + array.size());
+                    configuration.setSeries(series);
+                    ui.push();
                 }
-                //System.out.println("ARRAYSIZE = " + array.size());
-                configuration.setSeries(series);
-                ui.push();
             });
             interrupt();
         } catch (Exception e) { //Interrupted

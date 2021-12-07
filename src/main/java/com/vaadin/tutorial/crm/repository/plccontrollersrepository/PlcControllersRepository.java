5cd59b26273cd1173f3738a3140109d3143db28f
Roman Grytsay <r.grytsay@platinka.ru>
2021-12-07 08:49:43 +0300

    Добавил sql запрос для выборки данных по контроллеру по его id

--- src/main/java/com/vaadin/tutorial/crm/repository/plccontrollersrepository/PlcControllersRepository.java
+++ src/main/java/com/vaadin/tutorial/crm/repository/plccontrollersrepository/PlcControllersRepository.java
@@ -3,6 +3,7 @@ package com.vaadin.tutorial.crm.repository.plccontrollersrepository;
 import com.vaadin.tutorial.crm.entity.plccontrollersentity.PlcControllers;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -14,4 +15,7 @@ import java.util.List;
 public interface PlcControllersRepository extends JpaRepository<PlcControllers, Long> {
     @Query("select pc from plccontrollers pc where pc.delete = 0")
     List<PlcControllers> getAll();
+
+    @Query("select pc from plccontrollers pc where pc.id = :id and pc.delete = 0")
+    List<PlcControllers> getAllByID(@Param("id") Long id);
 }

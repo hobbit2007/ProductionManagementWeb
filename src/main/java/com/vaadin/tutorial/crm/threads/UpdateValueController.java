8c63b6195ac260ceaa2958d1f8878b0a23535e7c
Roman Grytsay <r.grytsay@platinka.ru>
2021-12-07 13:14:18 +0300

    Исправил логику потока

--- src/main/java/com/vaadin/tutorial/crm/threads/UpdateValueController.java
+++ src/main/java/com/vaadin/tutorial/crm/threads/UpdateValueController.java
@@ -3,13 +3,9 @@ package com.vaadin.tutorial.crm.threads;
 import com.sourceforge.snap7.moka7.S7;
 import com.sourceforge.snap7.moka7.S7Client;
 import com.vaadin.flow.component.UI;
-import com.vaadin.flow.component.html.Label;
 import com.vaadin.flow.component.textfield.TextField;
 import com.vaadin.tutorial.crm.entity.plccontrollersentity.SignalList;
-import com.vaadin.tutorial.crm.model.DataFromPlc;
-import com.vaadin.tutorial.crm.service.plccontrollersservice.SchedulerService;
 
-import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -18,27 +14,36 @@ import java.util.List;
 public class UpdateValueController extends Thread {
     private final UI ui;
     private  List<TextField> element;
-    private List<DataFromPlc> array;
+    private List<SignalList> array;
+    private S7Client s7Client;
+    byte[] buffer = new byte[65536];
 
-    public UpdateValueController(UI ui, List<TextField> element, List<DataFromPlc> array) {//int db, int pos, int offset, String controllerIpAddress
+    public UpdateValueController(UI ui, List<TextField> element, List<SignalList> array, S7Client s7Client) {//int db, int pos, int offset, String controllerIpAddress
         this.ui = ui;
         this.element = element;
         this.array = array;
+        this.s7Client = s7Client;
     }
 
     @Override
     public void run() {
-        try {
-            ui.access(() -> {
-                for (int i = 0; i < element.size(); i++) {
-                    element.get(i).setValue(String.valueOf(array.get(i).getValue()));//dataFromPlcList.get(0).getValue())
-                    //System.out.println("FROM THREAD = " + array.get(i).getValue() + "SIZE ELEMENT = " + element.size() + "SIZE VALUE = " + array.size());
-                    ui.push();
-                }
-            });
-            interrupt();
-            } catch (Exception e) { //Interrupted
-                e.printStackTrace();
+
+        while (!isInterrupted()) {
+            try {
+                ui.access(() -> {
+                    for (int i = 0; i < element.size(); i++) {
+                        s7Client.ReadArea(S7.S7AreaDB, array.get(i).getDbValue(), 0, array.get(i).getPosition() + array.get(i).getOffset(), buffer);
+                        float readData = S7.GetFloatAt(buffer, array.get(i).getPosition());
+                        element.get(i).setValue(String.valueOf(readData));
+                        //System.out.println("FROM THREAD [" + getId() + " - " + getName()+ "] =" + readData);
+                        ui.push();
+                    }
+                });
+                sleep(2000);
+            } catch (InterruptedException e) { //Interrupted
+                break;
+                //e.printStackTrace();
+            }
         }
     }
 }

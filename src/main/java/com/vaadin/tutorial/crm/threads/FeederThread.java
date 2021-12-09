3b27767518814eb1fd66b373bb8867e2796c30e8
Roman Grytsay <r.grytsay@platinka.ru>
2021-12-09 13:04:55 +0300

    Класс поток - проверяет подключение к контроллерам

--- src/main/java/com/vaadin/tutorial/crm/threads/FeederThread.java
+++ src/main/java/com/vaadin/tutorial/crm/threads/FeederThread.java
@@ -1,37 +1,70 @@
 package com.vaadin.tutorial.crm.threads;
 
+import com.sourceforge.snap7.moka7.S7;
+import com.sourceforge.snap7.moka7.S7Client;
 import com.vaadin.flow.component.UI;
 import com.vaadin.flow.component.html.Label;
+import com.vaadin.tutorial.crm.entity.plccontrollersentity.PlcControllers;
 import com.vaadin.tutorial.crm.service.plccontrollersservice.PLCConnect;
 
+import java.util.List;
+
 /**
- * Класс поток - проверяет подключение к контроллерам каждые 10 сек.
+ * Класс поток - проверяет подключение к контроллерам каждые 3 сек.
  */
 public class FeederThread extends Thread {
-    private final UI ui;
-    private  final Label element;
+    //private final UI ui;
+    //private  final Label element;
+    public static S7Client[] client = new S7Client[100];
+    List<PlcControllers> array;
 
     private int count = 30;
 
 
-    public FeederThread(UI ui, Label element) {
-        this.ui = ui;
-        this.element = element;
+    public FeederThread(List<PlcControllers> array) { //UI ui, Label element
+        //this.ui = ui;
+        //this.element = element;
+        this.array = array;
     }
 
     @Override
     public void run() {
         while (!interrupted()){ //count>-1
             try {
-                Thread.sleep(5000);
-                ui.access(()-> {
-                    element.setText(String.valueOf(PLCConnect.anyControllersStatus()));
-                    element.getStyle().set("color", "red");
-                    element.getStyle().set("font-weight", "bold");
-                    element.getStyle().set("font-size", "11pt");
-                    ui.push();
-                });
-                count--;
+                for (int i = 0; i < array.size(); i++) {
+                    client[i] = new S7Client();
+                    client[i].SetConnectionType(S7.OP);
+                    client[i].ConnectTo(array.get(i).getIp(), 0, 1);
+                    if (!client[i].Connected) {
+                        if (array.get(i).getId() == 1)
+                            PLCConnect.contrConnectedWashing = false;
+                        if (array.get(i).getId() == 2)
+                            PLCConnect.contrConnectedDiffusion = false;
+                        if (array.get(i).getId() == 3)
+                            PLCConnect.contrConnectedFermentation = false;
+                        if (array.get(i).getId() == 4)
+                            PLCConnect.contrConnected = false;
+                        if (array.get(i).getId() == 5)
+                            PLCConnect.contrConnectedBottling = false;
+                        if (array.get(i).getId() == 6)
+                            PLCConnect.contrConnectedDrying = false;
+                    }
+                    else {
+                        if (array.get(i).getId() == 1)
+                            PLCConnect.contrConnectedWashing = true;
+                        if (array.get(i).getId() == 2)
+                            PLCConnect.contrConnectedDiffusion = true;
+                        if (array.get(i).getId() == 3)
+                            PLCConnect.contrConnectedFermentation = true;
+                        if (array.get(i).getId() == 4)
+                            PLCConnect.contrConnected = true;
+                        if (array.get(i).getId() == 5)
+                            PLCConnect.contrConnectedBottling = true;
+                        if (array.get(i).getId() == 6)
+                            PLCConnect.contrConnectedDrying = true;
+                    }
+                }
+                sleep(3000);
             } catch (InterruptedException e) {
                 break;
             }

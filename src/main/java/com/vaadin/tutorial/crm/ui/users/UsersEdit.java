64969a4584f14ca00174c2b803f52f60a81c77c4
Roman Grytsay <r.grytsay@platinka.ru>
2021-12-09 15:29:22 +0300

    Добавил обновление пользователя

--- src/main/java/com/vaadin/tutorial/crm/ui/users/UsersEdit.java
+++ src/main/java/com/vaadin/tutorial/crm/ui/users/UsersEdit.java
@@ -3,11 +3,15 @@ package com.vaadin.tutorial.crm.ui.users;
 import com.vaadin.flow.component.dependency.CssImport;
 import com.vaadin.flow.component.grid.Grid;
 import com.vaadin.flow.component.grid.GridVariant;
+import com.vaadin.flow.component.html.Div;
+import com.vaadin.flow.component.notification.Notification;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.data.provider.ListDataProvider;
 import com.vaadin.flow.router.PageTitle;
 import com.vaadin.flow.router.Route;
 import com.vaadin.tutorial.crm.entity.User;
+import com.vaadin.tutorial.crm.service.DepartmentService;
+import com.vaadin.tutorial.crm.service.ShopService;
 import com.vaadin.tutorial.crm.service.UserService;
 import com.vaadin.tutorial.crm.ui.component.AnyComponent;
 import com.vaadin.tutorial.crm.ui.layout.UserLayout;
@@ -25,11 +29,17 @@ public class UsersEdit extends VerticalLayout {
     private Grid<User> grid = new Grid<>();
     private Grid.Column<User> colLogin, colFio, colPosition, colEmail, colDateCreate, colLastDate, colRole;
     private ListDataProvider<User> dataProvider;
+    private FormUsersEdit formUsersEdit;
+    private Div content;
+    private long userID;
     private UserService userService;
+    private DepartmentService departmentService;
 
     @Autowired
-    public UsersEdit(UserService userService) {
+    public UsersEdit(UserService userService, DepartmentService departmentService) {
         this.userService = userService;
+        this.departmentService = departmentService;
+
         AnyComponent anyComponent = new AnyComponent();
 
         addClassName("list-view");
@@ -37,13 +47,21 @@ public class UsersEdit extends VerticalLayout {
 
         configureGrid();
 
-        vMain.add(anyComponent.labelTitle("Редактирование пользователя"), grid);
-        vMain.setSizeFull();
+        vMain.add(anyComponent.labelTitle("Редактирование пользователя"));
         vMain.setDefaultHorizontalComponentAlignment(Alignment.CENTER);
 
-        add(vMain);
+        formUsersEdit = new FormUsersEdit(userService.getAll(), departmentService.getAllByAll());
+        formUsersEdit.addListener(FormUsersEdit.ContactFormEvent.EditEvent.class, e -> saveUser());
+        formUsersEdit.addListener(FormUsersEdit.ContactFormEvent.CloseEvent.class, e -> close());
+
+        content = new Div(grid, formUsersEdit);
+        content.addClassName("content");
+        content.setSizeFull();
+
+        add(vMain, content);
 
         updateDataGrid();
+        close();
     }
 
     private void configureGrid() {
@@ -68,11 +86,49 @@ public class UsersEdit extends VerticalLayout {
         colDateCreate.setResizable(true);
         colLastDate.setResizable(true);
         colRole.setResizable(true);
+
+        grid.asSingleSelect().addValueChangeListener(event -> {
+            if (event.getValue() != null) {
+                userID = event.getValue().getId();
+                editForm(event.getValue());
+            }
+        });
     }
 
     private void updateDataGrid() {
         dataProvider = new ListDataProvider<>(
                 userService.getAll());
         grid.setItems(dataProvider);
+    }
+
+    private void saveUser() {
+        try {
+            User user = new User();
+            user.setFio(FormUsersEdit.ContactFormEvent.EditEvent.getFio());
+            user.setEmail(FormUsersEdit.ContactFormEvent.EditEvent.getEmail());
+            user.setRole(FormUsersEdit.ContactFormEvent.EditEvent.getRole());
+            user.setId(userID);
+
+            userService.updateUserInfo(user);
+            Notification.show("Сохранено!", 5000, Notification.Position.MIDDLE);
+            close();
+        }
+        catch (Exception e) {
+
+        }
+    }
+
+    private void close() {
+        formUsersEdit.setUsers(null);
+        formUsersEdit.setVisible(false);
+        updateDataGrid();
+    }
+
+    private void editForm(User user) {
+        if (user == null)
+            close();
+        formUsersEdit.setUsers(user);
+        formUsersEdit.setVisible(true);
+        addClassName("editing");
     }
 }

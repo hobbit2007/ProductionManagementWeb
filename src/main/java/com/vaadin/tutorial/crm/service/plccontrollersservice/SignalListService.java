3d25e228e5f7cce7eb2cad6c53e64cbe0a580511
r.grytsay <roman-grytsay@yandex.ru>
2021-12-10 13:19:12 +0300

    Добавил сессию

--- src/main/java/com/vaadin/tutorial/crm/service/plccontrollersservice/SignalListService.java
+++ src/main/java/com/vaadin/tutorial/crm/service/plccontrollersservice/SignalListService.java
@@ -1,6 +1,7 @@
 package com.vaadin.tutorial.crm.service.plccontrollersservice;
 
 import com.vaadin.tutorial.crm.entity.plccontrollersentity.SignalList;
+import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -8,6 +9,7 @@ import java.util.List;
 /**
  * Класс модель содержащий методы для работы с таблицей signallist
  */
+@Scope("session")
 @Service
 public interface SignalListService {
     List<SignalList> findSignalList(Long controllerId);

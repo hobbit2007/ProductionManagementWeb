34dfe11a1410f0d6ffcc1b0acb186809e2b94724
r.grytsay <roman-grytsay@yandex.ru>
2021-12-10 13:19:29 +0300

    Добавил сессию

--- src/main/java/com/vaadin/tutorial/crm/service/plccontrollersservice/PlcValueService.java
+++ src/main/java/com/vaadin/tutorial/crm/service/plccontrollersservice/PlcValueService.java
@@ -1,12 +1,14 @@
 package com.vaadin.tutorial.crm.service.plccontrollersservice;
 
 import com.vaadin.tutorial.crm.entity.plccontrollersentity.PlcValue;
+import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Service;
 import java.util.List;
 
 /**
  * Класс интерфейс содержащий методы для работы с таблицей plcwashing101
  */
+@Scope("session")
 @Service
 public interface PlcValueService {
     List<PlcValue> getSignalOnController(Long controllerId);

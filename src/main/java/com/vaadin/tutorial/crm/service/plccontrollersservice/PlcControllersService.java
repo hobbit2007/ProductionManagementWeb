b81609c53dec969d410f2cc710850fe85fa006ff
r.grytsay <roman-grytsay@yandex.ru>
2021-12-10 13:19:51 +0300

    Добавил сессию

--- src/main/java/com/vaadin/tutorial/crm/service/plccontrollersservice/PlcControllersService.java
+++ src/main/java/com/vaadin/tutorial/crm/service/plccontrollersservice/PlcControllersService.java
@@ -1,6 +1,7 @@
 package com.vaadin.tutorial.crm.service.plccontrollersservice;
 
 import com.vaadin.tutorial.crm.entity.plccontrollersentity.PlcControllers;
+import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -8,6 +9,7 @@ import java.util.List;
 /**
  * Класс интерфейс содержит методы для работы с таблицей plccontrollers
  */
+@Scope("session")
 @Service
 public interface PlcControllersService {
     List<PlcControllers> getAll();

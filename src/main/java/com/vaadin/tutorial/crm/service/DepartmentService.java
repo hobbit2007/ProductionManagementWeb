4515a25cd1ea229ab48d67e8cddc502fce69e870
r.grytsay <roman-grytsay@yandex.ru>
2021-12-10 13:18:57 +0300

    Добавил сессию

--- src/main/java/com/vaadin/tutorial/crm/service/DepartmentService.java
+++ src/main/java/com/vaadin/tutorial/crm/service/DepartmentService.java
@@ -1,6 +1,7 @@
 package com.vaadin.tutorial.crm.service;
 
 import com.vaadin.tutorial.crm.entity.Department;
+import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -8,6 +9,7 @@ import java.util.List;
 /**
  * Класс интерфейс содержащий методы для работы с таблицей department
  */
+@Scope("session")
 @Service
 public interface DepartmentService {
     List<Department> getAll(Long shopId);

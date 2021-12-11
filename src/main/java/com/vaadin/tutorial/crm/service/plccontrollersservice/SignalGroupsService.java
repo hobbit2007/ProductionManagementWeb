57b97f8e077673605fb3189e0330519cd8edab69
r.grytsay <roman-grytsay@yandex.ru>
2021-12-11 13:56:08 +0300

    Добавил аннотацию

--- src/main/java/com/vaadin/tutorial/crm/service/plccontrollersservice/SignalGroupsService.java
+++ src/main/java/com/vaadin/tutorial/crm/service/plccontrollersservice/SignalGroupsService.java
@@ -1,12 +1,14 @@
 package com.vaadin.tutorial.crm.service.plccontrollersservice;
 
 import com.vaadin.tutorial.crm.entity.plccontrollersentity.SignalGroup;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 
 /**
  * Класс интерфейс содержащий методы для работы с таблицей groups
  */
+@Service
 public interface SignalGroupsService {
     List<SignalGroup> getAll();
 }

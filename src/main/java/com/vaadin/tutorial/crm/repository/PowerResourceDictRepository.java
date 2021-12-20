3bdccc6621d19768a7f94b83bff46fd942fa5f3f
Roman Grytsay <r.grytsay@platinka.ru>
2021-12-20 13:24:11 +0300

    Класс репозиторий содержащий sql запросы к таблице power_resource_dict

---
+++ src/main/java/com/vaadin/tutorial/crm/repository/PowerResourceDictRepository.java
@@ -0,0 +1,17 @@
+package com.vaadin.tutorial.crm.repository;
+
+import com.vaadin.tutorial.crm.entity.PowerResourceDict;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+/**
+ * Класс репозиторий содержащий sql запросы к таблице power_resource_dict
+ */
+@Repository
+public interface PowerResourceDictRepository extends JpaRepository<PowerResourceDict, Long> {
+    @Query("select prd from power_resource_dict prd where prd.delete = 0")
+    List<PowerResourceDict> getAll();
+}

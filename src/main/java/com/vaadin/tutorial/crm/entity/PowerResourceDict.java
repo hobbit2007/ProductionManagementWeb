dae9b68b9948330922889e226f3ecf0dcd2bbd7a
Roman Grytsay <r.grytsay@platinka.ru>
2021-12-20 13:16:18 +0300

    Класс модель описывающий таблицу power_resource_dict

---
+++ src/main/java/com/vaadin/tutorial/crm/entity/PowerResourceDict.java
@@ -0,0 +1,27 @@
+package com.vaadin.tutorial.crm.entity;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import java.io.Serializable;
+import java.util.Date;
+
+/**
+ * Класс модель описывающий таблицу power_resource_dict
+ */
+@Entity(name = "power_resource_dict")
+@Getter
+@Setter
+public class PowerResourceDict implements Serializable {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+    private String resourceName;
+    private Long idUser;
+    private Date dateCreate;
+    private Long delete;
+}

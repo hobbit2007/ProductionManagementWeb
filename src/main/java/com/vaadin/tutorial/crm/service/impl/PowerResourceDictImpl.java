b8951556c47e78d2976c500034eaaa70ef951738
Roman Grytsay <r.grytsay@platinka.ru>
2021-12-20 13:33:09 +0300

    Класс реализующий методы интерфейса PowerResourceDictService

---
+++ src/main/java/com/vaadin/tutorial/crm/service/impl/PowerResourceDictImpl.java
@@ -0,0 +1,32 @@
+package com.vaadin.tutorial.crm.service.impl;
+
+import com.vaadin.flow.component.notification.Notification;
+import com.vaadin.tutorial.crm.entity.PowerResourceDict;
+import com.vaadin.tutorial.crm.repository.PowerResourceDictRepository;
+import com.vaadin.tutorial.crm.service.PowerResourceDictService;
+
+import java.util.List;
+
+/**
+ * Класс реализующий методы интерфейса PowerResourceDictService
+ */
+public class PowerResourceDictImpl implements PowerResourceDictService {
+    private final PowerResourceDictRepository powerResourceDictRepository;
+
+    public PowerResourceDictImpl(PowerResourceDictRepository powerResourceDictRepository) {
+        this.powerResourceDictRepository = powerResourceDictRepository;
+    }
+
+    @Override
+    public List<PowerResourceDict> getAll() {
+        return powerResourceDictRepository.getAll();
+    }
+
+    @Override
+    public void saveAll(PowerResourceDict powerResourceDict) {
+        if (powerResourceDict != null)
+            powerResourceDictRepository.saveAndFlush(powerResourceDict);
+        else
+            Notification.show("Нет данных для записи!", 5000, Notification.Position.MIDDLE);
+    }
+}

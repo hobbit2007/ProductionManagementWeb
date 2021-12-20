af9c9c1dfd61307a50f40aae6d528bf15084407a
Roman Grytsay <r.grytsay@platinka.ru>
2021-12-20 13:28:46 +0300

    Класс интерфейс содержащий методы для работы с таблицей power_resource_dict

---
+++ src/main/java/com/vaadin/tutorial/crm/service/PowerResourceDictService.java
@@ -0,0 +1,17 @@
+package com.vaadin.tutorial.crm.service;
+
+
+import com.vaadin.tutorial.crm.entity.PowerResourceDict;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+/**
+ * Класс интерфейс содержащий методы для работы с таблицей power_resource_dict
+ */
+@Service
+public interface PowerResourceDictService {
+    List<PowerResourceDict> getAll();
+
+    void saveAll(PowerResourceDict powerResourceDict);
+}

76a06f1b3fb04c5d261b419611fa89b87e45477c
Roman Grytsay <r.grytsay@platinka.ru>
2021-12-20 13:20:56 +0300

    Класс модель описывающий таблицу power_resources

---
+++ src/main/java/com/vaadin/tutorial/crm/entity/PowerResources.java
@@ -0,0 +1,29 @@
+package com.vaadin.tutorial.crm.entity;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.io.Serializable;
+import java.util.Date;
+
+/**
+ * Класс модель описывающий таблицу power_resources
+ */
+@Entity(name = "power_resources")
+@Getter
+@Setter
+public class PowerResources implements Serializable {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+    private Long idPowerResource;
+    private double value;
+    private Long idUser;
+    private Date dateCreate;
+    private Long delete;
+
+    @ManyToOne
+    @JoinColumn(name = "idPowerResource", referencedColumnName = "id", insertable = false, updatable = false)
+    private PowerResourceDict powerResourceDict;
+}

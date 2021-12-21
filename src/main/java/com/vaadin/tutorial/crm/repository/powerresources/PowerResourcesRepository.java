b7257e66fbd60d28f1f6d52b91e893453f53b06b
Roman Grytsay <r.grytsay@platinka.ru>
2021-12-21 09:09:21 +0300

    Добавил sql запрос для выборки списка показаний по конкретному ресурсу

--- src/main/java/com/vaadin/tutorial/crm/repository/powerresources/PowerResourcesRepository.java
+++ src/main/java/com/vaadin/tutorial/crm/repository/powerresources/PowerResourcesRepository.java
@@ -3,6 +3,7 @@ package com.vaadin.tutorial.crm.repository.powerresources;
 import com.vaadin.tutorial.crm.entity.powerresources.PowerResources;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -14,4 +15,8 @@ import java.util.List;
 public interface PowerResourcesRepository extends JpaRepository<PowerResources, Long> {
     @Query("select pr from power_resources pr where pr.delete = 0")
     List<PowerResources> getAll();
+
+    //Выбираем список показаний по конкретному ресурсу
+    @Query("select pr from power_resources pr where pr.idPowerResource = :resourceID and pr.delete = 0")
+    List<PowerResources> getAllByResourceId(@Param("resourceID") String resourceID);
 }

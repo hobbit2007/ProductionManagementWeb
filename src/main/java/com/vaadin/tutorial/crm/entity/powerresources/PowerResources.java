c1c7ec80921c9a53c31d8ec63a194feefc78522c
Roman Grytsay <r.grytsay@platinka.ru>
2021-12-21 12:58:00 +0300

    Добавил объекты для заполнения treegrid

--- src/main/java/com/vaadin/tutorial/crm/entity/powerresources/PowerResources.java
+++ src/main/java/com/vaadin/tutorial/crm/entity/powerresources/PowerResources.java
@@ -3,10 +3,13 @@ package com.vaadin.tutorial.crm.entity.powerresources;
 import com.vaadin.tutorial.crm.entity.powerresources.PowerResourceDict;
 import lombok.Getter;
 import lombok.Setter;
+import org.hibernate.annotations.NotFound;
+import org.hibernate.annotations.NotFoundAction;
 
 import javax.persistence.*;
 import java.io.Serializable;
 import java.sql.Date;
+import java.time.OffsetTime;
 
 /**
  * Класс модель описывающий таблицу power_resources
@@ -18,13 +21,35 @@ public class PowerResources implements Serializable {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
-    private Long idPowerResource;
+    private long idPowerResource;
     private double value;
     private Long idUser;
     private Date dateCreate;
     private Long delete;
+    private OffsetTime time;
+
+    @Transient
+    String description;
 
     @ManyToOne
     @JoinColumn(name = "idPowerResource", referencedColumnName = "id", insertable = false, updatable = false)
     private PowerResourceDict powerResourceDict;
+
+    @NotFound(action = NotFoundAction.IGNORE)
+    @ManyToOne
+    @JoinColumn(name = "idPowerResource", referencedColumnName = "id", insertable = false, updatable = false)
+    private PowerResources parent;
+
+    public PowerResources() {}
+
+    public PowerResources(String description, PowerResources parent) {
+        this.description = description;
+        this.parent = parent;
+    }
+
+    public PowerResources(double value, Date dateCreate, PowerResources parent) {
+        this.value = value;
+        this.dateCreate = dateCreate;
+        this.parent = parent;
+    }
 }

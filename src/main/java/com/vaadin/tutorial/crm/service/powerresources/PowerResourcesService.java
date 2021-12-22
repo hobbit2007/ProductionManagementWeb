ee926bd4c2a2f4ef270c6f666202ed7d3b73e425
Roman Grytsay <r.grytsay@platinka.ru>
2021-12-22 13:02:35 +0300

    Добавил метод, который выбирает список показаний за указанные даты

--- src/main/java/com/vaadin/tutorial/crm/service/powerresources/PowerResourcesService.java
+++ src/main/java/com/vaadin/tutorial/crm/service/powerresources/PowerResourcesService.java
@@ -3,6 +3,7 @@ package com.vaadin.tutorial.crm.service.powerresources;
 import com.vaadin.tutorial.crm.entity.powerresources.PowerResources;
 import org.springframework.stereotype.Service;
 
+import java.sql.Date;
 import java.util.List;
 
 /**
@@ -15,4 +16,6 @@ public interface PowerResourcesService {
     void saveAll(PowerResources powerResources);
 
     List<PowerResources> getAllByResourceId(Long resourceID);
+
+    List<PowerResources> getResourceBySearch(Date dateBegin, Date dateEnd);
 }

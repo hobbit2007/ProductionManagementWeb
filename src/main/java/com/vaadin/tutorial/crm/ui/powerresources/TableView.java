5b8fe2b43cadb4a57f5949d3212d2dcf0e5ec564
Roman Grytsay <r.grytsay@platinka.ru>
2021-12-22 15:57:26 +0300

    Добавил сортировку по дате

--- src/main/java/com/vaadin/tutorial/crm/ui/powerresources/TableView.java
+++ src/main/java/com/vaadin/tutorial/crm/ui/powerresources/TableView.java
@@ -1,10 +1,16 @@
 package com.vaadin.tutorial.crm.ui.powerresources;
 
+import com.vaadin.flow.component.button.Button;
+import com.vaadin.flow.component.datepicker.DatePicker;
 import com.vaadin.flow.component.dependency.CssImport;
 import com.vaadin.flow.component.grid.Grid;
 import com.vaadin.flow.component.grid.GridVariant;
+import com.vaadin.flow.component.notification.Notification;
+import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.component.treegrid.TreeGrid;
+import com.vaadin.flow.data.provider.hierarchy.TreeData;
+import com.vaadin.flow.data.provider.hierarchy.TreeDataProvider;
 import com.vaadin.flow.router.PageTitle;
 import com.vaadin.flow.router.Route;
 import com.vaadin.tutorial.crm.entity.powerresources.PowerResourceDict;
@@ -13,7 +19,8 @@ import com.vaadin.tutorial.crm.service.powerresources.PowerResourceDictService;
 import com.vaadin.tutorial.crm.service.powerresources.PowerResourcesService;
 import com.vaadin.tutorial.crm.ui.component.AnyComponent;
 import com.vaadin.tutorial.crm.ui.layout.PowerLayout;
-
+import java.sql.Date;
+import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -27,20 +34,67 @@ import java.util.List;
 @CssImport(value = "./styles/vaadin-text-field-styles.css", themeFor = "vaadin-text-field")
 public class TableView extends VerticalLayout {
     VerticalLayout vMain = new VerticalLayout();
+    HorizontalLayout hSort = new HorizontalLayout();
     TreeGrid<PowerResources> grid;
     Grid.Column<PowerResources> colDesc, colValue, colDate, colTime;
+    DatePicker dateBegin = new DatePicker("с:");
+    DatePicker dateEnd = new DatePicker("по:");
+    Button sortButton = new Button("Сортировка");
+    Button allViewButton = new Button("Показать все");
     private final PowerResourcesService powerResourcesService;
     private final PowerResourceDictService powerResourceDictService;
     public TableView(PowerResourcesService powerResourcesService, PowerResourceDictService powerResourceDictService) {
         this.powerResourcesService = powerResourcesService;
         this.powerResourceDictService = powerResourceDictService;
+
         setSizeFull();
         configureGrid();
         updateList();
-        vMain.add(new AnyComponent().labelTitle("Таблица показаний"), grid);
+
+        dateBegin.setI18n(new AnyComponent().datePickerRus());
+        dateEnd.setI18n(new AnyComponent().datePickerRus());
+        //dateEnd.setValue(LocalDate.now());
+
+        hSort.add(new AnyComponent().labelTitle("Сортировка по дате:"), dateBegin, dateEnd, sortButton, allViewButton);
+        hSort.setDefaultVerticalComponentAlignment(Alignment.BASELINE);
+
+        vMain.add(new AnyComponent().labelTitle("Таблица показаний"), hSort, grid);
         vMain.setDefaultHorizontalComponentAlignment(Alignment.CENTER);
         vMain.setSizeFull();
         add(vMain);
+
+        sortButton.addClickListener(e -> {
+            if (!dateBegin.isEmpty() && !dateEnd.isEmpty()) {
+                if (Date.valueOf(dateBegin.getValue()).getTime() <= Date.valueOf(dateEnd.getValue()).getTime()) {
+                    if (Date.valueOf(dateEnd.getValue()).getTime() <= new java.util.Date().getTime()) {
+                        TreeData<PowerResources> treeData = new TreeData<>();
+                        TreeDataProvider<PowerResources> dataProvider = new TreeDataProvider<>(treeData);
+                        grid.setDataProvider(dataProvider);
+                        fillTreeGrid(powerResourcesService.getResourceBySearch(Date.valueOf(dateBegin.getValue().toString()), Date.valueOf(dateEnd.getValue().toString())));
+                        dataProvider.refreshAll();
+                    }
+                    else {
+                        Notification.show("Дата окончания не может быть больше текущей!", 3000, Notification.Position.MIDDLE);
+                        return;
+                    }
+                }
+                else {
+                    Notification.show("Дата начала не может быть больше даты окончания!", 3000, Notification.Position.MIDDLE);
+                    return;
+                }
+            }
+            else {
+                Notification.show("Не выбраны даты для сортировки!", 3000, Notification.Position.MIDDLE);
+                return;
+            }
+        });
+        allViewButton.addClickListener(e -> {
+            TreeData<PowerResources> treeData = new TreeData<>();
+            TreeDataProvider<PowerResources> dataProvider = new TreeDataProvider<>(treeData);
+            grid.setDataProvider(dataProvider);
+            fillTreeGrid(powerResourcesService.getAll());
+            dataProvider.refreshAll();
+        });
     }
     private void fillTreeGrid(List<PowerResources> data) {
         boolean parentFlag = false;//Проверяем пустая родительская группа или нет, если нет, то группу в таблице не показываем

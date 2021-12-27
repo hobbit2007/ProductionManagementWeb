cff5cc2ef6f153d3947a68e99cb75d2364075894
r.grytsay <roman-grytsay@yandex.ru>
2021-12-27 12:19:55 +0300

    Класс репозиторий содержащий sql запросы к таблице settings

---
+++ src/main/java/com/vaadin/tutorial/crm/repository/writetodb/WriteToDBRepository.java
@@ -0,0 +1,17 @@
+package com.vaadin.tutorial.crm.repository.writetodb;
+
+import com.vaadin.tutorial.crm.entity.writetodb.WriteToDB;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+/**
+ * Класс репозиторий содержащий sql запросы к таблице settings
+ */
+@Repository
+public interface WriteToDBRepository extends JpaRepository<WriteToDB, Long> {
+    @Query("select s from settings s where s.delete = 0")
+    List<WriteToDB> getAll();
+}

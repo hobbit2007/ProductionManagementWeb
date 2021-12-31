3a4c3c8c6c296aa4b3e94bc86d1222b3d6c4f079
r.grytsay <roman-grytsay@yandex.ru>
2021-12-31 16:11:11 +0300

    Исправил поле сортировки

--- src/main/java/com/vaadin/tutorial/crm/repository/powerresources/PowerResourcesRepository.java
+++ src/main/java/com/vaadin/tutorial/crm/repository/powerresources/PowerResourcesRepository.java
@@ -20,7 +20,7 @@ public interface PowerResourcesRepository extends JpaRepository<PowerResources, 
     List<PowerResources> getAll();
 
     //Выбираем список показаний по конкретному ресурсу
-    @Query("select pr from power_resources pr where pr.idPowerResource = :resourceID and pr.delete = 0 order by pr.id asc")
+    @Query("select pr from power_resources pr where pr.idPowerResource = :resourceID and pr.delete = 0 order by pr.dateCreate asc")
     List<PowerResources> getAllByResourceId(@Param("resourceID") Long resourceID);
 
     //Выбирает список показаний за указанные даты

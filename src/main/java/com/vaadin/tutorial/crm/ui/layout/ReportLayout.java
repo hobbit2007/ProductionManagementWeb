2ce0b5c72ba2ba70734f50cfecb5013c1a931283
Roman Grytsay <r.grytsay@platinka.ru>
2022-01-03 16:47:37 +0300

    Класс реализующий шапку и боковое меню для Отчетов

---
+++ src/main/java/com/vaadin/tutorial/crm/ui/layout/ReportLayout.java
@@ -0,0 +1,45 @@
+package com.vaadin.tutorial.crm.ui.layout;
+
+import com.vaadin.flow.component.applayout.AppLayout;
+import com.vaadin.flow.component.icon.Icon;
+import com.vaadin.flow.component.icon.VaadinIcon;
+import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
+import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+import com.vaadin.flow.router.HighlightConditions;
+import com.vaadin.flow.router.RouterLink;
+import com.vaadin.tutorial.crm.security.SecurityConfiguration;
+import com.vaadin.tutorial.crm.ui.MainView;
+import com.vaadin.tutorial.crm.ui.component.LabelComponent;
+import com.vaadin.tutorial.crm.ui.report.PowerResourceReport;
+
+/**
+ * Класс реализующий шапку и боковое меню для Отчетов
+ */
+public class ReportLayout extends AppLayout {
+    private final SecurityConfiguration securityConfiguration;
+    LabelComponent labelComponent;
+    HorizontalLayout hMenu1 = new HorizontalLayout();
+    HorizontalLayout hMenu2 = new HorizontalLayout();
+    public ReportLayout(SecurityConfiguration securityConfiguration) {
+        this.securityConfiguration = securityConfiguration;
+        createHeader();
+        createDrawer();
+    }
+    private void createHeader() {
+        labelComponent = new LabelComponent(securityConfiguration);
+        addToNavbar(labelComponent.labelHead());
+    }
+    private void createDrawer() {
+        RouterLink back = new RouterLink("Назад", MainView.class);
+        back.setHighlightCondition(HighlightConditions.sameLocation());
+        Icon icon1 = new Icon(VaadinIcon.ARROW_BACKWARD);
+        hMenu1.add(icon1, back);
+
+        RouterLink powerValue = new RouterLink("Отчет по показаниям", PowerResourceReport.class);
+        powerValue.setHighlightCondition(HighlightConditions.sameLocation());
+        Icon icon2 = new Icon(VaadinIcon.PIN_POST);
+        hMenu2.add(icon2, powerValue);
+
+        addToDrawer(new VerticalLayout(hMenu1, hMenu2));
+    }
+}

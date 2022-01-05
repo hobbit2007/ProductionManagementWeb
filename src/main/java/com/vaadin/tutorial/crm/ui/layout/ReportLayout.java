dd6edd70457df91bb893e240c9dd345cac91ec51
r.grytsay <roman-grytsay@yandex.ru>
2022-01-05 17:44:07 +0300

    Добавил новые кнопки в меню

--- src/main/java/com/vaadin/tutorial/crm/ui/layout/ReportLayout.java
+++ src/main/java/com/vaadin/tutorial/crm/ui/layout/ReportLayout.java
@@ -1,16 +1,22 @@
 package com.vaadin.tutorial.crm.ui.layout;
 
+import com.vaadin.flow.component.UI;
 import com.vaadin.flow.component.applayout.AppLayout;
+import com.vaadin.flow.component.button.Button;
 import com.vaadin.flow.component.icon.Icon;
 import com.vaadin.flow.component.icon.VaadinIcon;
 import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.router.HighlightConditions;
 import com.vaadin.flow.router.RouterLink;
+import com.vaadin.tutorial.crm.entity.powerresources.PowerReportModel;
 import com.vaadin.tutorial.crm.security.SecurityConfiguration;
+import com.vaadin.tutorial.crm.service.powerresources.PowerResourcesService;
 import com.vaadin.tutorial.crm.ui.MainView;
 import com.vaadin.tutorial.crm.ui.component.LabelComponent;
+import com.vaadin.tutorial.crm.ui.report.PowerReportEmpty;
 import com.vaadin.tutorial.crm.ui.report.PowerResourceReport;
+import org.springframework.beans.factory.annotation.Autowired;
 
 /**
  * Класс реализующий шапку и боковое меню для Отчетов
@@ -19,9 +25,15 @@ public class ReportLayout extends AppLayout {
     private final SecurityConfiguration securityConfiguration;
     LabelComponent labelComponent;
     HorizontalLayout hMenu1 = new HorizontalLayout();
-    HorizontalLayout hMenu2 = new HorizontalLayout();
-    public ReportLayout(SecurityConfiguration securityConfiguration) {
+
+    private final PowerResourcesService powerResourcesService;
+    private final PowerReportModel powerReportModel;
+    @Autowired
+    public ReportLayout(SecurityConfiguration securityConfiguration, PowerResourcesService powerResourcesService, PowerReportModel powerReportModel) {
         this.securityConfiguration = securityConfiguration;
+        this.powerResourcesService = powerResourcesService;
+        this.powerReportModel = powerReportModel;
+
         createHeader();
         createDrawer();
     }
@@ -35,11 +47,66 @@ public class ReportLayout extends AppLayout {
         Icon icon1 = new Icon(VaadinIcon.ARROW_BACKWARD);
         hMenu1.add(icon1, back);
 
-        RouterLink powerValue = new RouterLink("Отчет по показаниям", PowerResourceReport.class);
-        powerValue.setHighlightCondition(HighlightConditions.sameLocation());
+        Button powerWaterValue = new Button("Отчет вода");
         Icon icon2 = new Icon(VaadinIcon.PIN_POST);
-        hMenu2.add(icon2, powerValue);
+        powerWaterValue.setIcon(icon2);
+        powerWaterValue.addClickListener(e -> {
+            UI.getCurrent().navigate(PowerReportEmpty.class);
+            powerReportModel.setMsg("Отчет по использованию воды ");
+            powerReportModel.setId(1);
+            UI.getCurrent().navigate(new PowerResourceReport(powerResourcesService, powerReportModel).getClass());
+        });
 
-        addToDrawer(new VerticalLayout(hMenu1, hMenu2));
+        Button powerGasValue = new Button("Отчет газ  ");
+        Icon icon3 = new Icon(VaadinIcon.PIN_POST);
+        powerGasValue.setIcon(icon3);
+        powerGasValue.addClickListener(e -> {
+            UI.getCurrent().navigate(PowerReportEmpty.class);
+            powerReportModel.setMsg("Отчет по использованию газа");
+            powerReportModel.setId(4);
+            UI.getCurrent().navigate(new PowerResourceReport(powerResourcesService, powerReportModel).getClass());
+        });
+
+        Button powerEnter1Value = new Button("Отчет Ввод1");
+        Icon icon4 = new Icon(VaadinIcon.PIN_POST);
+        powerEnter1Value.setIcon(icon4);
+        powerEnter1Value.addClickListener(e -> {
+            UI.getCurrent().navigate(PowerReportEmpty.class);
+            powerReportModel.setMsg("Отчет по использованию электричества ввод1");
+            powerReportModel.setId(5);
+            UI.getCurrent().navigate(new PowerResourceReport(powerResourcesService, powerReportModel).getClass());
+        });
+
+        Button powerEnter2Value = new Button("Отчет Ввод2");
+        Icon icon5 = new Icon(VaadinIcon.PIN_POST);
+        powerEnter2Value.setIcon(icon5);
+        powerEnter2Value.addClickListener(e -> {
+            UI.getCurrent().navigate(PowerReportEmpty.class);
+            powerReportModel.setMsg("Отчет по использованию электричества ввод2");
+            powerReportModel.setId(6);
+            UI.getCurrent().navigate(new PowerResourceReport(powerResourcesService, powerReportModel).getClass());
+        });
+
+        Button powerTotalEnterValue = new Button("Отчет электроэнергия");
+        Icon icon6 = new Icon(VaadinIcon.PIN_POST);
+        powerTotalEnterValue.setIcon(icon6);
+        powerTotalEnterValue.addClickListener(e -> {
+            UI.getCurrent().navigate(PowerReportEmpty.class);
+            powerReportModel.setMsg("Отчет по использованию электроэнергия суммарно");
+            powerReportModel.setId(9);
+            UI.getCurrent().navigate(new PowerResourceReport(powerResourcesService, powerReportModel).getClass());
+        });
+
+        Button powerStockValue = new Button("Отчет стоки");
+        Icon icon7 = new Icon(VaadinIcon.PIN_POST);
+        powerStockValue.setIcon(icon7);
+        powerStockValue.addClickListener(e -> {
+            UI.getCurrent().navigate(PowerReportEmpty.class);
+            powerReportModel.setMsg("Отчет по стокам");
+            powerReportModel.setId(10);
+            UI.getCurrent().navigate(new PowerResourceReport(powerResourcesService, powerReportModel).getClass());
+        });
+
+        addToDrawer(new VerticalLayout(hMenu1, powerWaterValue, powerGasValue, powerEnter1Value, powerEnter2Value, powerTotalEnterValue, powerStockValue));
     }
 }

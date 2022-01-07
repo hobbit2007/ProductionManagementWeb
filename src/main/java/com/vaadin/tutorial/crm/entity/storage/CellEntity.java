31f56a82211862ad1c4701582988aede6011ebfc
r.grytsay <roman-grytsay@yandex.ru>
2022-01-07 11:22:30 +0300

    Класс модель описывающий поля таблицы cells

---
+++ src/main/java/com/vaadin/tutorial/crm/entity/storage/CellEntity.java
@@ -0,0 +1,28 @@
+package com.vaadin.tutorial.crm.entity.storage;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import java.io.Serializable;
+import java.util.Date;
+
+/**
+ * Класс модель описывающий поля таблицы cells
+ */
+@Entity(name = "cells")
+@Getter
+@Setter
+public class CellEntity implements Serializable {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+    private String cellName;
+    private long idStorage;
+    private long idUser;
+    private Date dateCreate;
+    private long delete;
+}

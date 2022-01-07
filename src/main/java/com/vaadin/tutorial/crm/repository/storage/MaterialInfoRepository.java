43d25b052c268ad397bee106a80f91ba446af85f
r.grytsay <roman-grytsay@yandex.ru>
2022-01-07 12:46:26 +0300

    Класс репозиторий содержащий sql запросы к таблице materialinfo

---
+++ src/main/java/com/vaadin/tutorial/crm/repository/storage/MaterialInfoRepository.java
@@ -0,0 +1,17 @@
+package com.vaadin.tutorial.crm.repository.storage;
+
+import com.vaadin.tutorial.crm.entity.storage.MaterialInfoEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+/**
+ * Класс репозиторий содержащий sql запросы к таблице materialinfo
+ */
+@Repository
+public interface MaterialInfoRepository extends JpaRepository<MaterialInfoEntity, Long> {
+    @Query("select mi from materialinfo mi where mi.delete = 0 order by mi.materialName asc")
+    List<MaterialInfoEntity> getAll();
+}

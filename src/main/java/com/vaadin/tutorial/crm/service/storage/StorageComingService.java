2d8551a343103faf0d7249e45f448f58fafdd324
r.grytsay <roman-grytsay@yandex.ru>
2022-01-07 15:59:23 +0300

    Класс интерфейс содержащий методы доступа к таблице storage_coming

---
+++ src/main/java/com/vaadin/tutorial/crm/service/storage/StorageComingService.java
@@ -0,0 +1,14 @@
+package com.vaadin.tutorial.crm.service.storage;
+
+import com.vaadin.tutorial.crm.entity.storage.StorageComingEntity;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+/**
+ * Класс интерфейс содержащий методы доступа к таблице storage_coming
+ */
+@Service
+public interface StorageComingService {
+    List<StorageComingEntity> getAll();
+}

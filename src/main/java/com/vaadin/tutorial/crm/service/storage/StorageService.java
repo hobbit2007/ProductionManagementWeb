ca1292d390bb05b643841cd16d4c524e9fa034c6
r.grytsay <roman-grytsay@yandex.ru>
2022-01-07 11:16:32 +0300

    Добавил аннотацию

--- src/main/java/com/vaadin/tutorial/crm/service/storage/StorageService.java
+++ src/main/java/com/vaadin/tutorial/crm/service/storage/StorageService.java
@@ -1,12 +1,14 @@
 package com.vaadin.tutorial.crm.service.storage;
 
 import com.vaadin.tutorial.crm.entity.storage.StorageEntity;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 
 /**
  * Класс интерфейс содержащий методы для доступа к таблице storage
  */
+@Service
 public interface StorageService {
     List<StorageEntity> getAll();
 }

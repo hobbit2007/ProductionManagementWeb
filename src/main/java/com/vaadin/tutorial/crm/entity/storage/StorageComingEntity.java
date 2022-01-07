bb5c7a3446e77efdae398704d6f273738735b263
r.grytsay <roman-grytsay@yandex.ru>
2022-01-07 15:42:43 +0300

    Класс модель описывающий поля таблицы storage_coming

---
+++ src/main/java/com/vaadin/tutorial/crm/entity/storage/StorageComingEntity.java
@@ -0,0 +1,29 @@
+package com.vaadin.tutorial.crm.entity.storage;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import java.io.Serializable;
+import java.util.Date;
+
+/**
+ * Класс модель описывающий поля таблицы storage_coming
+ */
+@Entity(name = "storage_coming")
+@Getter
+@Setter
+public class StorageComingEntity implements Serializable {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+    private long idMaterial;
+    private double qtyCome;
+    private long idMeas;
+    private long idUser;
+    private Date dateCreate;
+    private long delete;
+}

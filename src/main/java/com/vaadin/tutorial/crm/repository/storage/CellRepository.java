6c8d1fcccf5430386c9582e92daa21faa52a1968
r.grytsay <roman-grytsay@yandex.ru>
2022-01-07 11:25:10 +0300

    Класс репозиторий содержащий sql запросы к таблице cells

---
+++ src/main/java/com/vaadin/tutorial/crm/repository/storage/CellRepository.java
@@ -0,0 +1,17 @@
+package com.vaadin.tutorial.crm.repository.storage;
+
+import com.vaadin.tutorial.crm.entity.storage.CellEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+/**
+ * Класс репозиторий содержащий sql запросы к таблице cells
+ */
+@Repository
+public interface CellRepository extends JpaRepository<CellEntity, Long> {
+    @Query("select c from cells c where c.delete = 0")
+    List<CellEntity> getAll();
+}

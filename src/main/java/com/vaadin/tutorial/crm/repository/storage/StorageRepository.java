889f572971711419688ba7a2ef3e7e1a67f483fc
r.grytsay <roman-grytsay@yandex.ru>
2022-01-08 13:47:50 +0300

    Добавил sql запрос, который проверяет наличие склада в БД

--- src/main/java/com/vaadin/tutorial/crm/repository/storage/StorageRepository.java
+++ src/main/java/com/vaadin/tutorial/crm/repository/storage/StorageRepository.java
@@ -3,6 +3,7 @@ package com.vaadin.tutorial.crm.repository.storage;
 import com.vaadin.tutorial.crm.entity.storage.StorageEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -14,4 +15,8 @@ import java.util.List;
 public interface StorageRepository extends JpaRepository<StorageEntity, Long> {
     @Query("select s from storage s where s.delete = 0 order by s.storageName asc")
     List<StorageEntity> getAll();
+
+    //Запрос проверяет наличие склада в БД
+    @Query("select s from storage s where s.storageName = :storeName and s.delete = 0")
+    List<StorageEntity> getCheckStorage(@Param("storeName") String storeName);
 }

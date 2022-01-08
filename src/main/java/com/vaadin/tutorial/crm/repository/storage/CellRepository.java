f0ecd40c09b0ea26ed167d3173540a08912343a1
r.grytsay <roman-grytsay@yandex.ru>
2022-01-08 14:33:13 +0300

    Добавил sql запрос, который проверяет наличие ячейки в БД

--- src/main/java/com/vaadin/tutorial/crm/repository/storage/CellRepository.java
+++ src/main/java/com/vaadin/tutorial/crm/repository/storage/CellRepository.java
@@ -3,6 +3,7 @@ package com.vaadin.tutorial.crm.repository.storage;
 import com.vaadin.tutorial.crm.entity.storage.CellEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -14,4 +15,8 @@ import java.util.List;
 public interface CellRepository extends JpaRepository<CellEntity, Long> {
     @Query("select c from cells c where c.delete = 0 order by c.cellName asc")
     List<CellEntity> getAll();
+
+    //Запрос проверяет наличие ячейки в БД
+    @Query("select c from cells c where c.cellName = :cellName and c.delete = 0")
+    List<CellEntity> getCheckCell(@Param("cellName") String cellName);
 }

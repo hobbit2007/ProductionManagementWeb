461e397c0d14234ef0c672cc3f10c632c664a0d4
r.grytsay <roman-grytsay@yandex.ru>
2022-01-09 14:10:40 +0300

    Добавил sql запрос для проверки наличия объекта хранения по артикулу в БД

--- src/main/java/com/vaadin/tutorial/crm/repository/storage/MaterialInfoRepository.java
+++ src/main/java/com/vaadin/tutorial/crm/repository/storage/MaterialInfoRepository.java
@@ -3,6 +3,7 @@ package com.vaadin.tutorial.crm.repository.storage;
 import com.vaadin.tutorial.crm.entity.storage.MaterialInfoEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -14,4 +15,8 @@ import java.util.List;
 public interface MaterialInfoRepository extends JpaRepository<MaterialInfoEntity, Long> {
     @Query("select mi from materialinfo mi where mi.delete = 0 order by mi.materialName asc")
     List<MaterialInfoEntity> getAll();
+
+    //Проверяем наличие объекта хранения по артикулу в БД
+    @Query("select mi from materialinfo mi where mi.article = :article and mi.delete = 0")
+    List<MaterialInfoEntity> getCheckArticle(@Param(":article") String article);
 }

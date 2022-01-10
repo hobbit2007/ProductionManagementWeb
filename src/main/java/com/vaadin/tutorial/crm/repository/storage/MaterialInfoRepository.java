697cdcc9c1f6625781fa16f872229babb63c67f7
Roman Grytsay <r.grytsay@platinka.ru>
2022-01-10 11:02:58 +0300

    Добавил sql запрос, который обновляет некоторые значения в таблице materialinfo

--- src/main/java/com/vaadin/tutorial/crm/repository/storage/MaterialInfoRepository.java
+++ src/main/java/com/vaadin/tutorial/crm/repository/storage/MaterialInfoRepository.java
@@ -2,9 +2,11 @@ package com.vaadin.tutorial.crm.repository.storage;
 
 import com.vaadin.tutorial.crm.entity.storage.MaterialInfoEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
@@ -19,4 +21,13 @@ public interface MaterialInfoRepository extends JpaRepository<MaterialInfoEntity
     //Проверяем наличие объекта хранения по артикулу в БД
     @Query("select mi from materialinfo mi where mi.article = :article and mi.delete = 0")
     List<MaterialInfoEntity> getCheckArticle(@Param("article") String article);
+
+    //Обновляем некоторые значения в таблице materialinfo
+    @Modifying
+    @Transactional //Добавляем эту аннотацию, чтоб насильно сделать апдейт, даже если он запрещен
+    @Query("update materialinfo mi set mi.materialName = :materialName, mi.article = :article, mi.quantity = :qty, " +
+            "mi.costPrice = :costPrice, mi.marketPrice = :marketPrice " +
+            "where mi.id = :id")
+    void updateValue(@Param("materialName") String materialName, @Param("article") String article, @Param("qty") double qty,
+                        @Param("costPrice") double costPrice, @Param("marketPrice") double marketPrice, @Param("id") long id);
 }

84408ca97453d815dd1f35b62aff5d82d4ff9df1
Roman Grytsay <r.grytsay@platinka.ru>
2022-01-11 11:53:24 +0300

    Добавил реализацию метода для проверки наличия показаний в БД на выбранную дату

--- src/main/java/com/vaadin/tutorial/crm/service/impl/powerresources/PowerResourcesImpl.java
+++ src/main/java/com/vaadin/tutorial/crm/service/impl/powerresources/PowerResourcesImpl.java
@@ -79,4 +79,14 @@ public class PowerResourcesImpl implements PowerResourcesService {
     public List<PowerResources> getResourceBySort(Date dateBegin, Date dateEnd, Long id) {
         return powerResourcesRepository.getResourceBySort(dateBegin, dateEnd, id);
     }
+
+    /**
+     * Метод проверяет наличие показаний в БД на указанную дату
+     * @param dateValue - дата, выбранная при вводе показаний
+     * @return - возвращает список, если на выбранную дату присутствуют показания
+     */
+    @Override
+    public List<PowerResources> getCheckDate(Date dateValue) {
+        return powerResourcesRepository.getCheckDate(dateValue);
+    }
 }

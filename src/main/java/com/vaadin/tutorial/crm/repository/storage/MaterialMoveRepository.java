5d7a08a3449b06a5e601a914e50164de5221c83a
r.grytsay <roman-grytsay@yandex.ru>
2022-01-12 17:53:53 +0300

    Добавил sql запрос для поиска записи по id

--- src/main/java/com/vaadin/tutorial/crm/repository/storage/MaterialMoveRepository.java
+++ src/main/java/com/vaadin/tutorial/crm/repository/storage/MaterialMoveRepository.java
@@ -3,6 +3,7 @@ package com.vaadin.tutorial.crm.repository.storage;
 import com.vaadin.tutorial.crm.entity.storage.MaterialMoveEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -12,6 +13,10 @@ import java.util.List;
  */
 @Repository
 public interface MaterialMoveRepository extends JpaRepository<MaterialMoveEntity, Long> {
-    @Query("select mv from material_move mv where mv.delete = 0")
+    @Query("select mv from material_move mv where mv.writeoff = 0 and mv.delete = 0")
     List<MaterialMoveEntity> getAll();
+
+    //Поиск записи по id
+    @Query("select mv from material_move mv where mv.idMaterial = :idMaterial and mv.writeoff = 0 and mv.delete = 0")
+    List<MaterialMoveEntity> getAllByID(@Param("idMaterial") Long idMaterial);
 }

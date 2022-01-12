359f1b4e6e262248e49763f7c572be1424dd06c2
r.grytsay <roman-grytsay@yandex.ru>
2022-01-12 14:04:08 +0300

    Добавил sql запрос для поиска всех записей по id объекта хранения

--- src/main/java/com/vaadin/tutorial/crm/repository/storage/StorageComingRepository.java
+++ src/main/java/com/vaadin/tutorial/crm/repository/storage/StorageComingRepository.java
@@ -3,6 +3,7 @@ package com.vaadin.tutorial.crm.repository.storage;
 import com.vaadin.tutorial.crm.entity.storage.StorageComingEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -14,4 +15,8 @@ import java.util.List;
 public interface StorageComingRepository extends JpaRepository<StorageComingEntity, Long> {
     @Query("select sce from storage_coming sce where sce.delete = 0 order by sce.dateCreate asc")
     List<StorageComingEntity> getAll();
+
+    //Поиск всех записей по id объекта хранения
+    @Query("select sce from storage_coming sce where sce.delete = 0 and sce.idMaterial = :idMaterial order by sce.dateCreate asc")
+    List<StorageComingEntity> getAllByIdMaterial(@Param("idMaterial") Long idMaterial);
 }

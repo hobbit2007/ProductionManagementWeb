607f5e703dd0b1244f28d3e5af5f5947dea400a5
Roman Grytsay <r.grytsay@platinka.ru>
2022-01-13 15:17:25 +0300

    Класс интерфейс содержащий методы для доступа к данным таблицы price_change

---
+++ src/main/java/com/vaadin/tutorial/crm/service/impl/storage/ChangePriceService.java
@@ -0,0 +1,23 @@
+package com.vaadin.tutorial.crm.service.impl.storage;
+
+import com.vaadin.tutorial.crm.entity.storage.ChangePriceEntity;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+/**
+ * Класс интерфейс содержащий методы для доступа к данным таблицы price_change
+ */
+@Service
+public interface ChangePriceService {
+    List<ChangePriceEntity> getAll();
+
+    /**
+     * Метод выбирает историю изменения цены по id объекта хранения
+     * @param idMaterial - id объекта хранения
+     * @return - возвращает массив с историей изменения цены
+     */
+    List<ChangePriceEntity> getAllByIdMaterial(Long idMaterial);
+
+    void saveAll(ChangePriceEntity changePriceEntity);
+}

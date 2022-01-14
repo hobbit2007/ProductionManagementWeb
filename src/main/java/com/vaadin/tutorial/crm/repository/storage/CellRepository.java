cb7a0ff37cf6acc3c05ee40f891ddf8ac6ace771
Roman Grytsay <r.grytsay@platinka.ru>
2022-01-14 10:48:08 +0300

    Добавил sql запрос для обновления имени ячейки

--- src/main/java/com/vaadin/tutorial/crm/repository/storage/CellRepository.java
+++ src/main/java/com/vaadin/tutorial/crm/repository/storage/CellRepository.java
@@ -2,9 +2,11 @@ package com.vaadin.tutorial.crm.repository.storage;
 
 import com.vaadin.tutorial.crm.entity.storage.CellEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
@@ -23,4 +25,10 @@ public interface CellRepository extends JpaRepository<CellEntity, Long> {
     //Поиск ячейки по ID
     @Query("select c from cells c where c.id = :cellID and c.delete = 0 and c.idStorage = :idStorage")
     List<CellEntity> getFindCellByID(@Param("cellID") Long cellID, @Param("idStorage") Long idStorage);
+
+    //Обновляем имя ячейки
+    @Modifying
+    @Transactional
+    @Query("update cells c set c.cellName = :cellName where c.id = :id")
+    void updateCellName(@Param("cellName") String cellName, @Param("id") Long id);
 }

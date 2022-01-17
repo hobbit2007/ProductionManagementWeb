ada3beb939ea93fc218eb4c209ccbf381127a43a
Roman Grytsay <r.grytsay@platinka.ru>
2022-01-17 16:37:58 +0300

    Добавил обновления режимов записи и время повтора записи в БД

--- src/main/java/com/vaadin/tutorial/crm/ui/for1c/IntoToDB1C.java
+++ src/main/java/com/vaadin/tutorial/crm/ui/for1c/IntoToDB1C.java
@@ -22,63 +22,72 @@ import com.vaadin.tutorial.crm.ui.layout.For1CLayout;
 public class IntoToDB1C extends VerticalLayout {
     VerticalLayout vMain = new VerticalLayout();
     HorizontalLayout hMain1 = new HorizontalLayout();
-    Label alarmWashing = new Label();
-    Label header1Washing = new Label();
+    Label alarm1С = new Label();
+    Label header1С = new Label();
     Label timeLabel = new Label();
-    NumberField timeWashing = new NumberField();
-    RadioButtonGroup<String> radioButtonWashing = new RadioButtonGroup<>();
+    NumberField time1С = new NumberField();
+    RadioButtonGroup<String> radioButton1С = new RadioButtonGroup<>();
     private final WriteToDBService writeToDBService;
 
     public IntoToDB1C(WriteToDBService writeToDBService) {
         this.writeToDBService = writeToDBService;
-        radioButtonWashing.setItems("Нет", "Да");
-        radioButtonWashing.getStyle().set("color", "#d3b342");
-        radioButtonWashing.setValue(writeToDBService.getAll().get(8).getWriteOff());
+        radioButton1С.setItems("Нет", "Да");
+        radioButton1С.getStyle().set("color", "#d3b342");
+        radioButton1С.setValue(writeToDBService.getAll().get(8).getWriteOff());
 
-        header1Washing.setText(writeToDBService.getAll().get(8).getDescription());
-        header1Washing.getStyle().set("color", "#d3b342");
-        header1Washing.getStyle().set("font-weight", "bold");
-        header1Washing.getStyle().set("font-size", "13pt");
-        header1Washing.getStyle().set("margin-left", "30px");
+        header1С.setText(writeToDBService.getAll().get(8).getDescription());
+        header1С.getStyle().set("color", "#d3b342");
+        header1С.getStyle().set("font-weight", "bold");
+        header1С.getStyle().set("font-size", "13pt");
+        header1С.getStyle().set("margin-left", "30px");
 
         timeLabel.setText("Время повтора, мин.:");
         timeLabel.getStyle().set("color", "#d3b342");
         timeLabel.getStyle().set("font-weight", "bold");
         timeLabel.getStyle().set("font-size", "13pt");
 
-        timeWashing.setWidth("45px");
-        timeWashing.setValue((double) writeToDBService.getAll().get(8).getRepeatTime());
+        time1С.setWidth("45px");
+        time1С.setValue((double) writeToDBService.getAll().get(8).getRepeatTime());
 
-        if (radioButtonWashing.getValue().equals("Нет")) {
-            alarmDisable(alarmWashing);
+        if (radioButton1С.getValue().equals("Нет")) {
+            alarmDisable(alarm1С);
         }
         else {
-            alarmEnabled(alarmWashing);
+            alarmEnabled(alarm1С);
         }
 
-        radioButtonWashing.addValueChangeListener(e -> {
+        radioButton1С.addValueChangeListener(e -> {
             if (e.getValue().equals("Да")) {
-                alarmEnabled(alarmWashing);
+                alarmEnabled(alarm1С);
+                writeToDBService.updateWriteFor1C("Да");
             }
             if (e.getValue().equals("Нет")) {
-                alarmDisable(alarmWashing);
+                alarmDisable(alarm1С);
+                writeToDBService.updateWriteFor1C("Нет");
             }
         });
 
-        hMain1.add(header1Washing, radioButtonWashing, timeLabel, timeWashing, alarmWashing);
+        time1С.addValueChangeListener(e -> {
+            if (e.getValue() != null) {
+                if (!time1С.isEmpty())
+                    writeToDBService.updateRTFor1C(time1С.getValue().longValue());
+           }
+        });
+
+        hMain1.add(header1С, radioButton1С, timeLabel, time1С, alarm1С);
         hMain1.setVerticalComponentAlignment(Alignment.BASELINE);
         vMain.add(new AnyComponent().labelTitle("Управление записью в БД для 1С", "#d3b342", "15pt"), hMain1);
         vMain.setDefaultHorizontalComponentAlignment(Alignment.CENTER);
         add(vMain);
     }
     private void alarmDisable(Label label) {
-        label.setText("Запись отключена");
+        label.setText("Запись в БД отключена");
         label.getStyle().set("color", "red");
         label.getStyle().set("font-weight", "bold");
         label.getStyle().set("font-size", "13pt");
     }
     private void alarmEnabled(Label label) {
-        label.setText("Запись включена");
+        label.setText("Запись в БД включена");
         label.getStyle().set("color", "green");
         label.getStyle().set("font-weight", "bold");
         label.getStyle().set("font-size", "13pt");

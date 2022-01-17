f6d66dba3c85a9bf62067ccf49af0b585dc4c125
r.grytsay <roman-grytsay@yandex.ru>
2022-01-17 19:38:59 +0300

    Класс интерфейс содержащий методы для работы с таблицей for1cerp

---
+++ src/main/java/com/vaadin/tutorial/crm/service/for1c/For1CService.java
@@ -0,0 +1,25 @@
+package com.vaadin.tutorial.crm.service.for1c;
+
+import com.vaadin.tutorial.crm.entity.for1c.For1CEntity;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+/**
+ * Класс интерфейс содержащий методы для работы с таблицей for1cerp
+ */
+@Service
+public interface For1CService {
+    List<For1CEntity> getAll();
+
+    /**
+     * Метод для записи данных ПЛК контроллеров для их последующей вкпкдачи в 1С
+     * @param for1CEntity - объект класс For1CEntity
+     */
+    void saveAll(For1CEntity for1CEntity);
+
+    /**
+     * Метод для обновления поля delete после того как данные были переданы в 1С считаем их удаленными
+     */
+    void updateAfterLoadTo1C();
+}

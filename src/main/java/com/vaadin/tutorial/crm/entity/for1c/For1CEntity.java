df589c6c4ed03dac833e20258232bf5c9719fe14
Roman Grytsay <r.grytsay@platinka.ru>
2022-01-17 09:23:31 +0300

    Класс модель описывающий структуру таблицы for1cerp

---
+++ src/main/java/com/vaadin/tutorial/crm/entity/for1c/For1CEntity.java
@@ -0,0 +1,24 @@
+package com.vaadin.tutorial.crm.entity.for1c;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.util.Date;
+
+/**
+ * Класс модель описывающий структуру таблицы for1cerp
+ */
+@Entity(name = "for1cerp")
+@Getter
+@Setter
+public class For1CEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private long idOrderNum;
+    private long delete;
+    private Float value;
+    private String units;
+    private Date dateTime;
+}

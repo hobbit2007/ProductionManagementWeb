8a047655365e7bb3b6de2e8250fbe88630b03bc0
Roman Grytsay <r.grytsay@platinka.ru>
2022-01-17 16:07:40 +0300

    Класс реализующий шапку и боковое меню для меню Интеграция 1С

---
+++ src/main/java/com/vaadin/tutorial/crm/ui/layout/For1CLayout.java
@@ -0,0 +1,48 @@
+package com.vaadin.tutorial.crm.ui.layout;
+
+import com.vaadin.flow.component.applayout.AppLayout;
+import com.vaadin.flow.component.icon.Icon;
+import com.vaadin.flow.component.icon.VaadinIcon;
+import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
+import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+import com.vaadin.flow.router.HighlightConditions;
+import com.vaadin.flow.router.RouterLink;
+import com.vaadin.tutorial.crm.security.SecurityConfiguration;
+import com.vaadin.tutorial.crm.ui.MainView;
+import com.vaadin.tutorial.crm.ui.component.LabelComponent;
+import com.vaadin.tutorial.crm.ui.powerresources.TableView;
+
+/**
+ * Класс реализующий шапку и боковое меню для меню Интеграция 1С
+ */
+public class For1CLayout extends AppLayout {
+    private final SecurityConfiguration securityConfiguration;
+    LabelComponent labelComponent;
+    HorizontalLayout hMenu1 = new HorizontalLayout();
+    HorizontalLayout hMenu2 = new HorizontalLayout();
+
+    public For1CLayout(SecurityConfiguration securityConfiguration) {
+        this.securityConfiguration = securityConfiguration;
+
+        createHeader();
+        createDrawer();
+    }
+    private void createHeader() {
+        labelComponent = new LabelComponent(securityConfiguration);
+        addToNavbar(labelComponent.labelHead());
+    }
+    private void createDrawer() {
+        RouterLink back = new RouterLink("Назад", MainView.class);
+        back.setHighlightCondition(HighlightConditions.sameLocation());
+        Icon icon1 = new Icon(VaadinIcon.ARROW_BACKWARD);
+        hMenu1.add(icon1, back);
+
+        RouterLink for1cView = new RouterLink("Проверка интеграции", TableView.class);
+        for1cView.setHighlightCondition(HighlightConditions.sameLocation());
+        Icon icon2 = new Icon(VaadinIcon.EXTERNAL_LINK);
+        hMenu2.add(icon2, for1cView);
+
+        addToDrawer(new VerticalLayout(hMenu1, hMenu2));
+    }
+
+}

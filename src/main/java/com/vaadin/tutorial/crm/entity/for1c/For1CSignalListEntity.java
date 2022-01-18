f4289807837695f40a9bab9357a234c26e3a74ad
Roman Grytsay <r.grytsay@platinka.ru>
2022-01-18 10:49:34 +0300

    Добавил метод связи между таблицами

--- src/main/java/com/vaadin/tutorial/crm/entity/for1c/For1CSignalListEntity.java
+++ src/main/java/com/vaadin/tutorial/crm/entity/for1c/For1CSignalListEntity.java
@@ -1,5 +1,6 @@
 package com.vaadin.tutorial.crm.entity.for1c;
 
+import com.vaadin.tutorial.crm.entity.plccontrollersentity.SignalList;
 import lombok.Getter;
 import lombok.Setter;
 import javax.persistence.*;
@@ -20,4 +21,8 @@ public class For1CSignalListEntity {
     private long idUser;
     private Date dateCreate;
     private long delete;
+
+    @ManyToOne
+    @JoinColumn(name = "idSignalName", referencedColumnName = "id", insertable = false, updatable = false)
+    private SignalList signalList;
 }

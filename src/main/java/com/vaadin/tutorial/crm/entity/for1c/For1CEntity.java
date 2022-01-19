3dc2b77d9d7151d094cf6029709c2583681a923d
Roman Grytsay <r.grytsay@platinka.ru>
2022-01-19 13:26:13 +0300

    Добавил конструкторы и связи между таблицами

--- src/main/java/com/vaadin/tutorial/crm/entity/for1c/For1CEntity.java
+++ src/main/java/com/vaadin/tutorial/crm/entity/for1c/For1CEntity.java
@@ -1,9 +1,14 @@
 package com.vaadin.tutorial.crm.entity.for1c;
 
+import com.vaadin.tutorial.crm.entity.powerresources.PowerResources;
 import lombok.Getter;
 import lombok.Setter;
+import org.hibernate.annotations.NotFound;
+import org.hibernate.annotations.NotFoundAction;
 
 import javax.persistence.*;
+import java.io.Serializable;
+import java.time.OffsetTime;
 import java.util.Date;
 
 /**
@@ -12,7 +17,7 @@ import java.util.Date;
 @Entity(name = "for1cerp")
 @Getter
 @Setter
-public class For1CEntity {
+public class For1CEntity implements Serializable {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
@@ -25,4 +30,23 @@ public class For1CEntity {
 
     @Transient
     String description;
+
+    @NotFound(action = NotFoundAction.IGNORE)
+    @ManyToOne
+    @JoinColumn(name = "idSignal", referencedColumnName = "id", insertable = false, updatable = false)
+    private For1CEntity parent;
+
+    public For1CEntity() {}
+
+    public For1CEntity(String description, For1CEntity parent) {
+        this.description = description;
+        this.parent = parent;
+    }
+
+    public For1CEntity(float value, Date datetime, long id, For1CEntity parent) {
+        this.value = value;
+        this.datetime = datetime;
+        this.id = id;
+        this.parent = parent;
+    }
 }

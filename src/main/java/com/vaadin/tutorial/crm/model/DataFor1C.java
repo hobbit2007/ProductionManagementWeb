85c49fbb4a92ccf44372c514b4d103b8e187a72c
Roman Grytsay <r.grytsay@platinka.ru>
2022-01-19 14:56:43 +0300

    Добавил конструктор

--- src/main/java/com/vaadin/tutorial/crm/model/DataFor1C.java
+++ src/main/java/com/vaadin/tutorial/crm/model/DataFor1C.java
@@ -16,4 +16,11 @@ public class DataFor1C {
     private String variableDescription;
     private float value;
     private Date dateTime;
+
+    public DataFor1C (String variableName, String variableDescription, float value, Date dateTime) {
+        this.variableName = variableName;
+        this.variableDescription = variableDescription;
+        this.value = value;
+        this.dateTime = dateTime;
+    }
 }

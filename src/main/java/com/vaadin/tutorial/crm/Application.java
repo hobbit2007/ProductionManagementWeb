1949ad39b7369a32de03abd515c0924d817217cb
Roman Grytsay <r.grytsay@platinka.ru>
2022-01-21 14:23:15 +0300

    Убрал импорты

--- src/main/java/com/vaadin/tutorial/crm/Application.java
+++ src/main/java/com/vaadin/tutorial/crm/Application.java
@@ -1,23 +1,17 @@
 package com.vaadin.tutorial.crm;
 
-import com.vaadin.flow.component.page.AppShellConfigurator;
-import com.vaadin.flow.component.page.Push;
-import com.vaadin.flow.shared.communication.PushMode;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.web.servlet.error.ErrorMvcAutoConfiguration;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
-import org.springframework.scheduling.annotation.EnableScheduling;
 
 /**
  * The entry point of the Spring Boot application.
  */
-//@EnableScheduling
 @SpringBootApplication(exclude = ErrorMvcAutoConfiguration.class)
 public class Application extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
     }
-
 }

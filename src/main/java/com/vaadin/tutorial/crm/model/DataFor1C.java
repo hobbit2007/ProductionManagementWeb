1bf763f88e3d095ae529cca11ec6efe391c487ca
Roman Grytsay <r.grytsay@platinka.ru>
2022-01-21 08:23:55 +0300

    Добавил поле

--- src/main/java/com/vaadin/tutorial/crm/model/DataFor1C.java
+++ src/main/java/com/vaadin/tutorial/crm/model/DataFor1C.java
@@ -4,6 +4,7 @@ import lombok.Getter;
 import lombok.Setter;
 
 import java.util.Date;
+import java.util.List;
 
 /**
  * Класс модель для массива, хранящего данные из средних значений переменных ПЛК контроллеров и даты
@@ -16,6 +17,7 @@ public class DataFor1C {
     private String variableDescription;
     private float value;
     private Date dateTime;
+    private List<DataFor1C> result;
 
     public DataFor1C (String variableName, String variableDescription, float value, Date dateTime) {
         this.variableName = variableName;

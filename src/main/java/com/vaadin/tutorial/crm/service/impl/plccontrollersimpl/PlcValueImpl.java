3082f7c6705e7c70da24389328bd8859ee99fa6e
Roman Grytsay <r.grytsay@platinka.ru>
2022-01-24 15:20:31 +0300

    Добавил реализацию метода записи значений ПЛК контроллера в БД

--- src/main/java/com/vaadin/tutorial/crm/service/impl/plccontrollersimpl/PlcValueImpl.java
+++ src/main/java/com/vaadin/tutorial/crm/service/impl/plccontrollersimpl/PlcValueImpl.java
@@ -1,5 +1,6 @@
 package com.vaadin.tutorial.crm.service.impl.plccontrollersimpl;
 
+import com.vaadin.flow.component.notification.Notification;
 import com.vaadin.tutorial.crm.entity.plccontrollersentity.PlcValue;
 import com.vaadin.tutorial.crm.repository.plccontrollersrepository.PlcValueRepository;
 import com.vaadin.tutorial.crm.service.plccontrollersservice.PlcValueService;
@@ -21,5 +22,13 @@ public class PlcValueImpl implements PlcValueService {
     @Override
     public List<PlcValue> getSignalOnController(Long controllerId) {
         return plcWashingRepository.getSignalOnController(controllerId);
+    }
+
+    @Override
+    public void saveAll(PlcValue plcValue) {
+        if (plcValue != null)
+            plcWashingRepository.saveAndFlush(plcValue);
+        else
+            Notification.show("Нет данных для записи!", 3000, Notification.Position.MIDDLE);
     }
 }

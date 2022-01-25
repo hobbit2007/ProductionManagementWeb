2fcb3c3eec46d27f37fb6177b1b49a6bf225ff87
Roman Grytsay <r.grytsay@platinka.ru>
2022-01-25 09:29:36 +0300

    Класс репозиторий содержащий sql запросы к таблице plccontrollervaluedifusion

---
+++ src/main/java/com/vaadin/tutorial/crm/repository/plccontrollersrepository/PlcDiffusionRepository.java
@@ -0,0 +1,20 @@
+package com.vaadin.tutorial.crm.repository.plccontrollersrepository;
+
+import com.vaadin.tutorial.crm.entity.plccontrollersentity.PlcDiffusionValue;
+import com.vaadin.tutorial.crm.entity.plccontrollersentity.PlcValue;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+/**
+ * Класс репозиторий содержащий sql запросы к таблице plccontrollervaluedifusion
+ */
+@Repository
+public interface PlcDiffusionRepository extends JpaRepository<PlcDiffusionValue, Long> {
+    @Query("select pcvd, sl from plccontrollervaluedifusion pcvd " +
+            "join signallist sl on sl.id = pcvd.idSignal " +
+            "where sl.delete = 0 and pcvd.delete = 0 order by pcvd.dateCreate asc")
+    List<PlcValue> getAllSignal();
+}

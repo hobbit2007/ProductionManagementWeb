9d1109f2fb48959a8abd31d117b7f85fbe20351e
Roman Grytsay <r.grytsay@platinka.ru>
2022-01-31 11:39:02 +0300

    Класс интерфейс содержащий реализацию методов интерфейса SupplierService

---
+++ src/main/java/com/vaadin/tutorial/crm/service/impl/storage/SupplierImpl.java
@@ -0,0 +1,34 @@
+package com.vaadin.tutorial.crm.service.impl.storage;
+
+import com.vaadin.flow.component.notification.Notification;
+import com.vaadin.tutorial.crm.entity.storage.SupplierEntity;
+import com.vaadin.tutorial.crm.repository.storage.SupplierRepository;
+import com.vaadin.tutorial.crm.service.storage.SupplierService;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+/**
+ * Класс интерфейс содержащий реализацию методов интерфейса SupplierService
+ */
+@Service
+public class SupplierImpl implements SupplierService {
+    private final SupplierRepository supplierRepository;
+
+    public SupplierImpl(SupplierRepository supplierRepository) {
+        this.supplierRepository = supplierRepository;
+    }
+
+    @Override
+    public List<SupplierEntity> getAll() {
+        return supplierRepository.getAll();
+    }
+
+    @Override
+    public void saveAll(SupplierEntity supplierEntity) {
+        if (supplierEntity != null)
+            supplierRepository.saveAndFlush(supplierEntity);
+        else
+            Notification.show("Нет данных для записи!", 3000, Notification.Position.MIDDLE);
+    }
+}

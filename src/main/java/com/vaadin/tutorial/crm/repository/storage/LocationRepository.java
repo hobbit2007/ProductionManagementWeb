0b880094d638afacae8bc578f39dbe8f4c932252
Roman Grytsay <r.grytsay@platinka.ru>
2022-01-31 10:20:37 +0300

    Добавил запрос для поиска локации по ID

--- src/main/java/com/vaadin/tutorial/crm/repository/storage/LocationRepository.java
+++ src/main/java/com/vaadin/tutorial/crm/repository/storage/LocationRepository.java
@@ -3,6 +3,7 @@ package com.vaadin.tutorial.crm.repository.storage;
 import com.vaadin.tutorial.crm.entity.storage.LocationEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -14,4 +15,8 @@ import java.util.List;
 public interface LocationRepository extends JpaRepository<LocationEntity, Long> {
     @Query("select sl from store_location sl where sl.delete = 0 order by sl.id asc")
     List<LocationEntity> getAll();
+
+    //Поиск локации по ID
+    @Query("select sl from store_location sl where sl.id = :locationID and sl.delete = 0")
+    List<LocationEntity> getFindLocationByID(@Param("locationID") Long locationID);
 }

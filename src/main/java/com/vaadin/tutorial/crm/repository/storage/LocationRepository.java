37534896719d118d30d3f52436959503317ac002
Roman Grytsay <r.grytsay@platinka.ru>
2022-01-31 14:53:26 +0300

    Добавил sql запрос, который обновляет имя и описание локации

--- src/main/java/com/vaadin/tutorial/crm/repository/storage/LocationRepository.java
+++ src/main/java/com/vaadin/tutorial/crm/repository/storage/LocationRepository.java
@@ -2,9 +2,11 @@ package com.vaadin.tutorial.crm.repository.storage;
 
 import com.vaadin.tutorial.crm.entity.storage.LocationEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
@@ -19,4 +21,10 @@ public interface LocationRepository extends JpaRepository<LocationEntity, Long> 
     //Поиск локации по ID
     @Query("select sl from store_location sl where sl.id = :locationID and sl.delete = 0")
     List<LocationEntity> getFindLocationByID(@Param("locationID") Long locationID);
+
+    //Обновляем имя и описание локации
+    @Modifying
+    @Transactional
+    @Query("update store_location sl set sl.locationName = :locationName, sl.locationDescription = :locationDescription where sl.id = :id")
+    void updateLocation(@Param("locationName") String locationName, @Param("id") Long id, @Param("locationDescription") String locationDescription);
 }

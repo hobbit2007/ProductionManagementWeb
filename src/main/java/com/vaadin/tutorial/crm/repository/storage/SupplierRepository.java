765aa4f8da6c2baea8d8726020aba94f91a34055
Roman Grytsay <r.grytsay@platinka.ru>
2022-02-01 09:49:21 +0300

    Добавил sql запрос, который обновляет имя поставщика и номер контракта

--- src/main/java/com/vaadin/tutorial/crm/repository/storage/SupplierRepository.java
+++ src/main/java/com/vaadin/tutorial/crm/repository/storage/SupplierRepository.java
@@ -2,9 +2,11 @@ package com.vaadin.tutorial.crm.repository.storage;
 
 import com.vaadin.tutorial.crm.entity.storage.SupplierEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
@@ -19,4 +21,10 @@ public interface SupplierRepository extends JpaRepository<SupplierEntity, Long> 
     //Запрос проверяет наличие поставщика в БД
     @Query("select s from supplier s where s.supplierName = :supplierName and s.delete = 0")
     List<SupplierEntity> getCheckSupplier(@Param("supplierName") String supplierName);
+
+    //Обновляем имя и номер контракта
+    @Modifying
+    @Transactional
+    @Query("update supplier s set s.supplierName = :supplierName, s.contract = :contract where s.id = :id")
+    void updateSupplier(@Param("supplierName") String supplierName, @Param("id") Long id, @Param("contract") String contract);
 }

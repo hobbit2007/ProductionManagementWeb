5fdf29df9348b16811e4d34ec909a4e593a36a37
Roman Grytsay <r.grytsay@platinka.ru>
2022-02-01 08:56:33 +0300

    Добавил sql запрос для проверки на существование локации в БД

--- src/main/java/com/vaadin/tutorial/crm/repository/storage/SupplierRepository.java
+++ src/main/java/com/vaadin/tutorial/crm/repository/storage/SupplierRepository.java
@@ -1,8 +1,10 @@
 package com.vaadin.tutorial.crm.repository.storage;
 
+import com.vaadin.tutorial.crm.entity.storage.LocationEntity;
 import com.vaadin.tutorial.crm.entity.storage.SupplierEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -14,4 +16,8 @@ import java.util.List;
 public interface SupplierRepository extends JpaRepository<SupplierEntity, Long> {
     @Query("select s from supplier s where s.delete = 0 order by s.supplierName asc")
     List<SupplierEntity> getAll();
+
+    //Запрос проверяет наличие поставщика в БД
+    @Query("select s from supplier s where s.supplierName = :supplierName and s.delete = 0")
+    List<SupplierEntity> getCheckSupplier(@Param("supplierName") String supplierName);
 }

6e645fc12698ed9e9b5e1b3c8971cc2c96345393
Roman Grytsay <r.grytsay@platinka.ru>
2022-02-01 08:56:53 +0300

    Добавил sql запрос для проверки на существование локации в БД

--- src/main/java/com/vaadin/tutorial/crm/repository/storage/SupplierRepository.java
+++ src/main/java/com/vaadin/tutorial/crm/repository/storage/SupplierRepository.java
@@ -1,6 +1,5 @@
 package com.vaadin.tutorial.crm.repository.storage;
 
-import com.vaadin.tutorial.crm.entity.storage.LocationEntity;
 import com.vaadin.tutorial.crm.entity.storage.SupplierEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;

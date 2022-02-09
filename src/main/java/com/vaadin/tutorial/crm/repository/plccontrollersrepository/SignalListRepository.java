61588df970bf5dbf20c95e1c3a8f6e43ffb6ce76
Roman Grytsay <r.grytsay@platinka.ru>
2022-02-09 10:26:12 +0300

    Добавил sql запрос, который обновляет некоторые значения в таблице signallist

--- src/main/java/com/vaadin/tutorial/crm/repository/plccontrollersrepository/SignalListRepository.java
+++ src/main/java/com/vaadin/tutorial/crm/repository/plccontrollersrepository/SignalListRepository.java
@@ -2,9 +2,11 @@ package com.vaadin.tutorial.crm.repository.plccontrollersrepository;
 
 import com.vaadin.tutorial.crm.entity.plccontrollersentity.SignalList;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
@@ -19,4 +21,14 @@ public interface SignalListRepository extends JpaRepository<SignalList, Long> {
     //Подсчитываем количество типов сигналов
     @Query("select idGroup from signallist where idController = :controllerID group by idGroup")
     Long countGroups(@Param("controllerID") Long controllerID);
+
+    //Обновляем некоторые значения в таблице signallist
+    @Modifying
+    @Transactional //Добавляем эту аннотацию, чтоб насильно сделать апдейт, даже если он запрещен
+    @Query("update signallist sl set sl.signalName = :signalName, sl.signalDescription = :signalDescription, " +
+            "sl.dbValue = :dbValue, sl.position = :position, sl.offset = :offset, sl.idGroup = :idGroup " +
+            "where sl.id = :id")
+    void updateValue(@Param("signalName") String signalName, @Param("signalDescription") String signalDescription,
+                     @Param("dbValue") Integer dbValue, @Param("position") Integer position,
+                     @Param("offset") Integer offset, @Param("idGroup") Long idGroup, @Param("id") long id);
 }
